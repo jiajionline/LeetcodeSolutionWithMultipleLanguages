4413de0914e85b16d0209bce0f37cb3ed99def56
jiajionline <Jiajionline@gmail.com>
2017-04-22 22:56:44 -0700

    add CommonClasses/ListNode

---
+++ CommonClasses/ListNode.cs
@@ -0,0 +1,9 @@
+﻿namespace LeetcodePracticeCsharpVersion
+{
+  public class ListNode
+    {
+      public int val;
+      public ListNode next;
+      public ListNode(int x) { val = x; }
+  }
+}
---
+++ CommonClasses/ListNode.py
@@ -0,0 +1,4 @@
+ class ListNode(object):
+     def __init__(self, x):
+         self.val = x
+         self.next = None
