ccf2b1ec03ff7aef2c5b97c9d98aabcde110e494
jiajionline <Jiajionline@gmail.com>
2017-05-02 20:23:07 -0700

    Remove Duplicates from Sorted List

---
+++ Remove Duplicates from Sorted List/RemoveDuplicatesfromSortedList.cs
@@ -0,0 +1,26 @@
+﻿namespace LeetcodePracticeCsharpVersion
+{
+    class RemoveDuplicatesfromSortedList
+    {
+        public ListNode DeleteDuplicates(ListNode head)
+        {
+            if (head == null || head.next == null) return head;
+
+            var current = head;
+
+            while (current != null)
+            {
+                var next = current.next;
+                while(next!=null && next.val == current.val)
+                {
+                    next = next.next;
+                }
+
+                current.next = next;               
+                current = next;
+            }
+
+            return head;
+        }
+    }
+}
