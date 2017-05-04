94ace67ba787b91e8984cbccb3e5f9245018c804
jiajionline <Jiajionline@gmail.com>
2017-05-03 22:34:04 -0700

    Find Largest Value in Each Tree Row

---
+++ Find Largest Value in Each Tree Row/FindLargestValueinEachTreeRow.py
@@ -0,0 +1,18 @@
+class FindLargestValueinEachTreeRow(object):
+    def largestValues(self, root):
+        currentLargestValueDict = dict()
+        self.DFS(root,1,currentLargestValueDict)
+        ret = list()
+        for i in range(0,len(currentLargestValueDict)):
+            ret.append(currentLargestValueDict[i+1])
+        return ret
+    
+    def DFS(self,node,level,largestValueDict):
+        if node is None:
+            return
+        if level in largestValueDict:
+            largestValueDict[level] = max(node.val, largestValueDict[level])
+        else:
+            largestValueDict[level] = node.val
+        self.DFS(node.left, level+1, largestValueDict)
+        self.DFS(node.right, level+1, largestValueDict)
