4f4714ee069e56ca989a33ec0a75a847815d5ffe
jiajionline <Jiajionline@gmail.com>
2017-05-09 16:45:29 -0700

    First Unique Character in a String

---
+++ First Unique Character in a String/FirstUniqueCharacterinaString.java
@@ -0,0 +1,24 @@
+
+public class FirstUniqueCharacterinaString {
+    public int firstUniqChar(String s)
+    {
+        if (s == null || s.length() == 0) return -1;
+        if (s.length() == 1) return 0;
+
+        int[] letterToCount = new int[26];
+        for(char c : s.toCharArray())
+        {
+            letterToCount[c - 'a']++;
+        }
+
+        for(int i =0;i<s.length();i++)
+        {
+            if(letterToCount[s.charAt(i) - 'a'] == 1)
+            {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+}
---
+++ First Unique Character in a String/FirstUniqueCharacterinaString.py
@@ -0,0 +1,16 @@
+class FirstUniqueChar(object):
+    def firstUniqChar(self, s):
+        if s is None or len(s)==0:
+            return -1
+        if len(s) == 1:
+            return 0
+        letterToCount = [0] * 26
+        for c in s:
+            letterToCount[ord(c)-ord('a')]+=1
+        for i in range(0,len(s)):
+            if letterToCount[ord(s[i])-ord('a')] == 1:
+                return i
+        return -1
+                
+        
+        
