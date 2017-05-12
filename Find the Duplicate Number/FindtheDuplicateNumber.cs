3f6343be5f9e0e8c2380b8ed151f8f009e528fcf
jiajionline <Jiajionline@gmail.com>
2017-05-11 22:03:48 -0700

    Find the Duplicate Number

---
+++ Find the Duplicate Number/FindtheDuplicateNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class FindtheDuplicateNumber
+    {
+        public int FindDuplicate(int[] nums)
+        {
+            for(int i = 0; i < nums.Length; i++)
+            {
+                var index = Math.Abs(nums[i]) - 1;
+                if (nums[index] < 0)
+                {
+                    return index + 1;
+                }
+                else
+                {
+                    nums[index] = 0 - nums[index];
+                }
+            }
+
+            return -1;
+        }
+    }
+}
