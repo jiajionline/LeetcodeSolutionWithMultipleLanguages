7d11e64caaac0242cc36e4a844bc53f0bd5230cd
jiajionline <Jiajionline@gmail.com>
2017-05-16 23:04:06 -0700

    Find the Difference

---
+++ Find the Difference/FindtheDifference.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class FindtheDifference
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            if(s == null && t!=null && t.Length == 1)
+            {
+                return t[0];
+            }
+            
+            int a = 0; int b = 0;
+            foreach(var c in s)
+            {
+                a ^= (int)c;
+            }
+
+            foreach(var c in t)
+            {
+                b ^= (int)c;
+            }
+
+            a ^= b;
+
+            return (char)a;
+        }
+    }
+}
