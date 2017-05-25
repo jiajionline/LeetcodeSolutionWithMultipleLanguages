10b93dd8dd08ed2691ea1e59fa479d3713162d57
jiajionline <Jiajionline@gmail.com>
2017-05-24 22:57:43 -0700

    Number Complement

---
+++ Number Complement/NumberComplement.cs
@@ -0,0 +1,17 @@
+﻿namespace LeetcodePracticeCsharpVersion
+{
+    class NumberComplement
+    {
+        public int FindComplement(int num)
+        {
+            var i = 1;
+            while(i < num)
+            {
+                i <<= 1;
+                i += 1;
+            }
+
+            return i - num;
+        }
+    }
+}
