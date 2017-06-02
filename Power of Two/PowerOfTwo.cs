53c38e050c0421a4f4b831435d32103658b9a48f
jiajionline <Jiajionline@gmail.com>
2017-06-01 21:46:31 -0700

    Power of Two

---
+++ Power of Two/PowerOfTwo.cs
@@ -0,0 +1,11 @@
+﻿
+namespace LeetcodePracticeCsharpVersion
+{
+    class PowerOfTwo
+    {
+        public bool IsPowerOfTwo(int n)
+        {
+            return (n > 0 && (n & (n - 1)) == 0);
+        }
+    }
+}
