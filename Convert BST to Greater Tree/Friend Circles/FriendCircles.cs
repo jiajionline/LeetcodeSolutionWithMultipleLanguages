966b153529063fcf916f112d044dbbe51c9ed9a7
jiajionline <Jiajionline@gmail.com>
2017-07-14 22:34:35 -0700

    Friend Circles

---
+++ Convert BST to Greater Tree/Friend Circles/FriendCircles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class FriendCircles
+    {
+        public int FindCircleNum(int[,] M)
+        {
+            bool[] visited = new bool[M.GetLength(0)];
+            int count = 0;
+            for (int i = 0; i < M.GetLength(0); i++)
+            {
+                if (visited[i] == false)
+                {
+                    DepthFirstSearch(M, visited, i);
+                    count++;
+                }
+            }
+            return count;
+        }
+        
+
+        private void DepthFirstSearch(int[,] M, bool[] visited, int i)
+        {
+            for(int j = 0; j < M.GetLength(1); j++)
+            {
+                if(M[i,j] == 1 && visited[j] == false)
+                {
+                        visited[j] = true;
+                        DepthFirstSearch(M, visited, j);
+                 }
+            }
+        }
+    }
+}
