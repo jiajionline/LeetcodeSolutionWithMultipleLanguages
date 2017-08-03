0959615ad1483709eaed6174d46038ee5a8ebcee
jiajionline <Jiajionline@gmail.com>
2017-08-02 22:24:18 -0700

    Valid Palindrome

---
+++ Valid Palindrome/ValidPalindrome.cs
@@ -0,0 +1,35 @@
+﻿namespace LeetcodePracticeCsharpVersion
+{
+    class ValidPalindrome
+    {
+        public bool IsPalindrome(string s)
+        {
+            if (string.IsNullOrEmpty(s)) return true;
+            if (s.Length == 1) return true;
+
+            int left = 0;
+            int right = s.Length - 1;
+
+            s = s.ToLowerInvariant();
+            while (left < right)
+            {
+                while (left < s.Length && !char.IsLetterOrDigit(s[left]))
+                {
+                    left++;
+                }
+
+                while (right >= 0 && !char.IsLetterOrDigit(s[right]))
+                {
+                    right--;
+                }
+
+                if (left > right) return true;
+                if (s[left] != s[right]) return false;
+                left++;
+                right--;
+            }
+
+            return true;
+        }
+    }
+}
