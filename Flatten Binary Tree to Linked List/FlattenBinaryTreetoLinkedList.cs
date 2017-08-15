fe10e6b1aec5186d579bfca2a7d8cb3cfaf233d6
jiajionline <Jiajionline@gmail.com>
2017-08-14 21:25:40 -0700

    Flatten Binary Tree to Linked List

---
+++ Flatten Binary Tree to Linked List/FlattenBinaryTreetoLinkedList.cs
@@ -0,0 +1,22 @@
+﻿namespace LeetcodePracticeCsharpVersion
+{
+    class FlattenBinaryTreetoLinkedList
+    {
+        public void Flatten(TreeNode root)
+        {
+            if (root == null) return;
+            var left = root.left;
+            var right = root.right;
+
+            root.left = null;
+            Flatten(left);
+            Flatten(right);
+
+            root.right = left;
+            TreeNode current = root;
+            while (current.right != null) current = current.right;
+
+            current.right = right;
+        }
+    }
+}
