533e72a5525983f88f7180f2f035f046a544b2d9
jiajionline <Jiajionline@gmail.com>
2017-08-14 22:10:03 -0700

    Remove Linked List Elements

---
+++ Remove Linked List Elements/RemoveLinkedListElements.cs
@@ -0,0 +1,27 @@
+﻿
+namespace LeetcodePracticeCsharpVersion
+{
+    class RemoveLinkedListElements
+    {
+        public ListNode RemoveElements(ListNode head, int val)
+        {
+            ListNode preHeader = new ListNode(-1);
+
+            ListNode current = preHeader;
+            ListNode index = head;
+
+            while (index != null)
+            {
+                if(index.val == val)
+                {
+                    current.next = index;
+                    current = current.next;
+                }
+
+                index = index.next;
+            }
+
+            return preHeader.next;
+        }
+    }
+}
