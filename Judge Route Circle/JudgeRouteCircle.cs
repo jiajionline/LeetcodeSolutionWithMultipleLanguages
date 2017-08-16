e0a8c7ad41af52ade1decb4c7e6f9930f67dafbf
jiajionline <Jiajionline@gmail.com>
2017-08-15 22:26:13 -0700

    Judge Route Circle

---
+++ Judge Route Circle/JudgeRouteCircle.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class JudgeRouteCircle
+    {
+        public bool JudgeCircle(string moves)
+        {
+            if (string.IsNullOrWhiteSpace(moves)) return true;
+            var upAndDownCount = 0;
+            var  leftAndRightCount = 0;
+
+            foreach(var c in moves)
+            {
+                switch (c)
+                {
+                    case 'U':
+                        upAndDownCount++;
+                        break;
+                    case 'D':
+                        upAndDownCount--;
+                        break;
+                    case 'L':
+                        leftAndRightCount--;
+                        break;
+                    case 'R':
+                        leftAndRightCount++;
+                        break;
+                    default:
+                        return false;
+                }
+
+            }
+
+            return upAndDownCount == 0 && leftAndRightCount == 0;
+        }
+    }
+}
