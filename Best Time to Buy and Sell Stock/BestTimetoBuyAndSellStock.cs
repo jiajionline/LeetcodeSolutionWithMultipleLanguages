6563ca3fefc261ff87557b894fb46b41336cf38c
jiajionline <Jiajionline@gmail.com>
2017-08-29 22:23:18 -0700

    Best Time to Buy and Sell Stock

---
+++ Best Time to Buy and Sell Stock/BestTimetoBuyAndSellStock.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class BestTimetoBuyAndSellStock
+    {
+        public int MaxProfit(int[] prices)
+        {
+            if (prices == null || prices.Length == 0) return 0;
+            int maxProfit = 0;
+            int currentMin = prices[0];
+
+            for(int i = 1; i < prices.Length; i++)
+            {
+                maxProfit = Math.Max(maxProfit, prices[i] - currentMin);
+                currentMin = Math.Min(currentMin, prices[i]);
+            }
+
+            return maxProfit;
+        }
+    }
+}
