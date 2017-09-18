73ad81d29bacacf9dd62a8747795dbb32ffd83c1
jiajionline <Jiajionline@gmail.com>
2017-09-17 21:37:34 -0700

    Binary Tree Preorder Traversal

---
+++ Binary Tree Preorder Traversal/BinaryTreePreorderTraversal.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetcodePracticeCsharpVersion
+{
+    class BinaryTreePreorderTraversal
+    {
+        public IList<int> PreorderTraversal(TreeNode root)
+        {
+            IList<int> ret = new List<int>();
+            if (root == null) return ret;
+            Stack<TreeNode> stack = new Stack<TreeNode>();
+            stack.Push(root);
+            while(stack.Count() > 0)
+            {
+                var node = stack.Pop();
+                ret.Add(node.val);
+                if (node.right != null) stack.Push(node.right);
+                if (node.left != null) stack.Push(node.left);
+            }
+
+            return ret;
+        }
+    }
+}
