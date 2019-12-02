19d17a22665e9dc709fbc505ca1c9a8f302a1b69
jiajionline <jiajionline@gmail.com>
2019-12-02 10:44:09 -0800

    upgrade LC 657

---
+++ 0657_Robot Return to Origin/RobotReturntoOrigin.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+    public bool JudgeCircle (string moves) {
+        var x = 0;
+        var y = 0;
+        foreach (var c in moves) {
+            switch (c) {
+                case 'U':
+                    y++;
+                    break;
+                case 'D':
+                    y--;
+                    break;
+                case 'L':
+                    x--;
+                    break;
+                case 'R':
+                    x++;
+                    break;
+            }
+        }
+
+        return x == 0 && y == 0;
+    }
+}
