9eb2f2b3cd24a6eeb6821bf623f78e853700f178
jiajionline <jiajionline@gmail.com>
2019-12-11 15:58:54 -0800

    Leetcode 973 K Closest Points to Origin

---
+++ 0973_K Closest Points to Origin/KClosestPointstoOrigin.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[][] kClosest(int[][] points, int K) {
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((p1, p2) -> p2[0] * p2[0] + p2[1] * p2[1] - p1[0] * p1[0] - p1[1] * p1[1]);
+        for (int[] p : points) {
+            pq.offer(p);
+            if (pq.size() > K) {
+                pq.poll();
+            }
+        }
+        int[][] res = new int[K][2];
+        while (K > 0) {
+            res[--K] = pq.poll();
+        }
+        return res;
+    }
+}
