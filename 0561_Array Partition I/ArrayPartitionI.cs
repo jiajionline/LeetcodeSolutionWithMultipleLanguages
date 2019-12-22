395d0e92d2206f9911149a5f9fa7f22ca518d392
jiajionline <jiajionline@gmail.com>
2019-12-22 07:45:59 -0800

    Leetcode 561 revise Array Partition I

---
+++ 0561_Array Partition I/ArrayPartitionI.cs
@@ -0,0 +1,10 @@
+﻿public class Solution {
+    public int ArrayPairSum(int[] nums) {
+        Array.Sort(nums);
+        int ans = 0;
+        for(int i=0;i<nums.Length;i=i+2){
+            ans += nums[i];
+        }
+        return ans;
+    }
+}
