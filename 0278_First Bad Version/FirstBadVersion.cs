886369b000c278f178aa3103b02d82fda0ffbdad
jiajionline <jiajionline@gmail.com>
2020-01-28 07:14:42 -0800

    Leetcode 278 First bad version

---
+++ 0278_First Bad Version/FirstBadVersion.cs
@@ -0,0 +1,18 @@
+/* The isBadVersion API is defined in the parent class VersionControl.
+      bool IsBadVersion(int version); */
+
+public class Solution : VersionControl {
+    public int FirstBadVersion(int n) {
+        int l = 0;
+        int r = n;
+        while(l < r){
+            int m = l + (r - l)/2;
+            if(IsBadVersion(m)){
+                r = m;
+            }else{
+                l = m + 1;
+            }
+        }
+        return l;
+    }
+}
