6b2034b25c1bb816a3c36608243521480744aed1
jiajionline <jiajionline@gmail.com>
2020-02-15 17:48:43 -0800

    Leetcode 207 Course Schedule

---
+++ 0207_Course Schedule/CourseSchedule.java
@@ -0,0 +1,42 @@
+class Solution {
+
+    private final static int VISITED = 1;
+    private final static int VISITING = 2;
+
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+       
+        List<List<Integer>> graph = new ArrayList<List<Integer>>(numCourses);
+        
+        for(int i=0;i<numCourses;i++){
+            graph.add(new ArrayList<Integer>());
+        }
+        for(int i=0;i<prerequisites.length;i++){
+            int course = prerequisites[i][0];
+            int preCourse = prerequisites[i][1];
+            graph.get(course).add(preCourse);
+        }
+
+        int[] visited = new int[numCourses];
+
+        for(int i=0;i<numCourses;i++){
+            if(DFS(i, graph, visited)) return false;
+        }
+
+        return true;
+    }
+
+    //return true if found cycle
+    private boolean DFS(int curr, List<List<Integer>> graph, int[] visited){
+        if(visited[curr] == VISITED) return false;
+        if(visited[curr] == VISITING) return true;
+
+        visited[curr] = VISITING;
+
+        for(int next : graph.get(curr)){
+            if(DFS(next, graph, visited)) return true;
+        }
+
+        visited[curr] = VISITED;
+        return false;
+    }
+}
