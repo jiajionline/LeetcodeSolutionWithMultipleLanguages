ebc574b9b4a981dced9f05f5f40385e9c1ce8d7f
jiajionline <jiajionline@gmail.com>
2020-02-18 11:59:18 -0800

    Leetcode 90 Subsets II

---
+++ 0090_Subsets II/SubsetsII.java
@@ -0,0 +1,27 @@
+class Solution {
+    public List<List<Integer>> subsetsWithDup(int[] nums) {
+        List<List<Integer>> ans = new ArrayList<List<Integer>>();
+        List<Integer> cur = new ArrayList<Integer>();
+        Arrays.sort(nums);
+        
+        for(int n=0;n<=nums.length;n++){
+            DFS(nums, 0,n,cur,ans);
+        }
+
+        return ans;
+    }
+
+    private void DFS(int[] nums, int s, int n, List<Integer> cur, List<List<Integer>> ans){
+        if(cur.size() == n){
+            ans.add(new ArrayList(cur));
+            return;
+        }
+
+        for(int i=s;i<nums.length;i++){
+            if(i>s && nums[i] == nums[i-1]) continue;
+            cur.add(nums[i]);
+            DFS(nums, i+1,n,cur,ans);
+            cur.remove(cur.size()-1);
+        }
+    }
+}
