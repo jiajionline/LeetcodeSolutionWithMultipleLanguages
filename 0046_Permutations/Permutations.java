2117b8e774b6a4592d9e7a3e3b7e84bf0b329293
jiajionline <jiajionline@gmail.com>
2020-02-18 19:36:55 -0800

    Leetcode 46 Permutations refactoring

--- 0046_Permutations/Permutations.java
+++ 0046_Permutations/Permutations.java
@@ -2,24 +2,24 @@ class Solution {
     public List<List<Integer>> permute(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
         List<Integer> curr = new ArrayList<Integer>();
-        boolean[] visited = new boolean[nums.length];
-        DFS(nums, 0, visited, curr, ans);
+        boolean[] used = new boolean[nums.length];
+        DFS(nums, 0, used, curr, ans);
         return ans;
     }
     
-    private void DFS(int[] nums, int s, boolean[] visited, List<Integer> curr, List<List<Integer>> ans){
+    private void DFS(int[] nums, int s, boolean[] used, List<Integer> curr, List<List<Integer>> ans){
         if(curr.size() == nums.length){
             ans.add(new ArrayList<Integer>(curr));
             return;
         }
         
         for(int i=0;i<nums.length;i++){
-            if(visited[i]) continue;
+            if(used[i]) continue;
             curr.add(nums[i]);
-            visited[i] = true;
-            DFS(nums, i+1, visited, curr, ans);
+            used[i] = true;
+            DFS(nums, i+1, used, curr, ans);
             curr.remove(curr.size()-1);
-            visited[i] = false;
+            used[i] = false;
         }
     }
 }
