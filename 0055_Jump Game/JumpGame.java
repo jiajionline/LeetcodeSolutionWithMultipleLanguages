5ce0c3957389c036291a4e8552768f182e462647
jiajionline <jiajionline@gmail.com>
2020-02-29 16:07:49 -0800

    Leetcode 55 Jump Game

---
+++ 0055_Jump Game/JumpGame.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        if(nums == null || nums.length == 0) return false;
+        //no need to jump
+        if(nums.length == 1) return true;
+        
+        int furthestPos = 0;
+        for(int i=0;i<nums.length;i++){
+            if(furthestPos < i){
+                return false;
+            }else if(furthestPos >= nums.length-1){
+                return true;
+            }else{
+                furthestPos = Math.max(furthestPos, nums[i] + i);
+            }
+        }
+        
+        return furthestPos >= nums.length;
+    }
+}
