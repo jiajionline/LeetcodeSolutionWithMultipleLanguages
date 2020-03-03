0f0cb77bdbf0ca594d3390e55adf1e60f55a0ff1
jiajionline <jiajionline@gmail.com>
2020-03-02 21:30:59 -0800

    Leetcode 322 Coin Change

---
+++ 0322_Coin Change/CoinChangeDFS.java
@@ -0,0 +1,26 @@
+class Solution {
+
+    private int ans = Integer.MAX_VALUE;
+
+    public int coinChange(int[] coins, int amount) {
+        if(coins == null || coins.length == 0) return 0;
+        coins = Arrays.stream(coins).boxed().sorted(Collections.reverseOrder()).mapToInt(Integer::intValue).toArray();
+        DFS(coins, 0, amount, 0);
+        return ans == Integer.MAX_VALUE ? -1 : ans;
+    }
+
+    private void DFS(int[] coins, int s, int amount, int count){
+        if(amount == 0){
+            ans = Math.min(ans, count);
+            return;
+        }
+
+        if(s == coins.length) return;
+
+        int coin = coins[s];
+        //k + count is a pruning, since we are trying to find the count which is less than ans
+        for(int k = amount/coin; k>=0 && k + count < ans;k--){
+            DFS(coins, s+1,amount - k * coin,count+k);
+        }
+    }
+}
