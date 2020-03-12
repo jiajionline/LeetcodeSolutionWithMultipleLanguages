314b4754639b970c138740242c6d25a6884521a4
jiajionline <jiajionline@gmail.com>
2020-03-12 07:19:16 -0700

    Leetcode 115 Distinct Subsequences

---
+++ 0115_Distinct Subsequences/DistinctSubsequences.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numDistinct(String s, String t) {
+        int[][] dp = new int[t.length()+1][s.length()+1];
+        for(int j=0;j<dp[0].length;j++){
+            dp[0][j] = 1;
+        }
+
+        for(int i=1;i<dp.length;i++){
+            for(int j=1;j<dp[i].length;j++){
+                if(s.charAt(j-1) == t.charAt(i-1)){
+                    dp[i][j] = dp[i-1][j-1] + dp[i][j-1];
+                }else{
+                    dp[i][j] = dp[i][j-1];
+                }
+            }
+        }
+
+        return dp[t.length()][s.length()];
+    }
+}
