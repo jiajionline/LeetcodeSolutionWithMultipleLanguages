695d9f4bcdae56d3e7e2a807ee4faf4d5496629a
jiajionline <jiajionline@gmail.com>
2020-03-14 10:49:39 -0700

    Leetcode 173 Binary Search Tree Iterator

---
+++ 0173_Binary Search Tree Iterator/BinarySearchTreeIterator.cs
@@ -0,0 +1,44 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int x) { val = x; }
+ * }
+ */
+public class BSTIterator {
+
+    private Stack<TreeNode> stack = new Stack<TreeNode>();
+    
+    public BSTIterator(TreeNode root) {
+        PushAllLeftChild(root);
+    }
+    
+    /** @return the next smallest number */
+    public int Next() {
+        var current = stack.Pop();
+        PushAllLeftChild(current.right);
+        return current.val;
+    }
+    
+    /** @return whether we have a next smallest number */
+    public bool HasNext() {
+        return stack.Count > 0;
+    }
+    
+    private void PushAllLeftChild(TreeNode node){
+        var current = node;
+        while(current!=null){
+            stack.Push(current);
+            current = current.left;
+        }
+    }
+}
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator obj = new BSTIterator(root);
+ * int param_1 = obj.Next();
+ * bool param_2 = obj.HasNext();
+ */
