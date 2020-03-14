8e24c7f3f793a11dc4fac1a221be7bc497e8c8b0
jiajionline <jiajionline@gmail.com>
2020-03-14 14:40:44 -0700

    Leetcode 485 Max Consecutive Ones

---
+++ 0485_Max Consecutive Ones/MaxConsecutiveOnes.cs
@@ -0,0 +1,16 @@
+﻿class Solution {
+    public int FindMaxConsecutiveOnes (int[] nums) {
+        int max = 0;
+        int currentCount = 0;
+        foreach (var v in nums) {
+            if (v == 1) {
+                currentCount++;
+                max = Math.Max (currentCount, max);
+            } else {
+                currentCount = 0;
+            }
+        }
+
+        return max;
+    }
+}
