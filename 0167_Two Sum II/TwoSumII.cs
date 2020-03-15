f8e4a0df5377355aa6047fd2bab5e19a87f07983
jiajionline <jiajionline@gmail.com>
2020-03-14 17:06:37 -0700

    Leetcode 167 Two Sum II Input array is sorted

---
+++ 0167_Two Sum II/TwoSumII.cs
@@ -0,0 +1,23 @@
+﻿public class Solution {
+    public int[] TwoSum(int[] numbers, int target) {
+        var ans = new int[2];
+        
+        int s = 0;
+        int e = numbers.Length - 1;
+        
+        while(s<e){
+            var sum = numbers[s] + numbers[e];
+            if(sum == target){
+                ans[0] = s+1;
+                ans[1] = e+1;
+                break;
+            }else if(sum > target){
+                e--;
+            }else{
+                s++;
+            }
+        }
+        
+        return ans;
+    }
+}
