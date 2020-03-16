22994260eeeae09d684d538bded669c2a598b716
jiajionline <jiajionline@gmail.com>
2020-03-15 17:50:50 -0700

    Leetcode 63 Unique Paths II

---
+++ 0063_Unique Paths II/UniquePathsII.cs
@@ -0,0 +1,22 @@
+public class Solution {
+   public int UniquePathsWithObstacles(int[,] obstacleGrid) {
+       var m = obstacleGrid.GetLength(0)+1;
+       var n = obstacleGrid.GetLength(1)+1;
+
+       var DP = new int[m,n];
+      
+       for(int i=1;i<m;i++){
+           for(int j=1;j<n;j++){
+              if(obstacleGrid[i-1,j-1] == 1){
+                  DP[i,j] = 0;
+              }else if(i==1 && j==1){
+                  DP[i,j] = 1;
+              }else{
+                  DP[i,j] = DP[i-1,j] + DP[i,j-1];
+              }
+           }
+       }
+
+       return DP[m-1,n-1];               
+   }
+}
