21a07fee0bfd4c109a641ee4f463681654dca1d7
jiajionline <jiajionline@gmail.com>
2020-03-24 07:20:14 -0700

    Leetcode 160 Intersection of Two Linked LIsts

---
+++ 0160_Intersection of Two Linked Lists/IntersectionofTwoLinkedLists.cs
@@ -0,0 +1,52 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public ListNode GetIntersectionNode (ListNode headA, ListNode headB) {
+        int a = Count (headA);
+        int b = Count (headB);
+
+        ListNode longP, shortP;
+        if (a > b) {
+            longP = headA;
+            shortP = headB;
+        } else {
+            longP = headB;
+            shortP = headA;
+        }
+
+        int m = Math.Abs (a - b);
+        while (m > 0) {
+            longP = longP.next;
+            m--;
+        }
+
+        while (longP != null && shortP != null) {
+            if (longP == shortP) return longP;
+            else {
+                longP = longP.next;
+                shortP = shortP.next;
+            }
+        }
+
+        return null;
+    }
+
+    private int Count (ListNode node) {
+        int count = 0;
+        if (node == null) return count;
+        var current = node;
+        count = 1;
+        while (current.next != null) {
+            count++;
+            current = current.next;
+        }
+
+        return count;
+    }
+}
