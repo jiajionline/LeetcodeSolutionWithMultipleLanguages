8b40349ed887ea8ba7e92e9b59010c1b3050ef5c
jiajionline <jiajionline@gmail.com>
2020-03-23 18:48:41 -0700

    Leetcode 35 Search Insert Position

--- 0035_Search Insert Position/SearchInsertPosition.java
+++ 0035_Search Insert Position/SearchInsertPosition.java
@@ -2,10 +2,10 @@ class Solution {
     public int searchInsert(int[] nums, int target) {
         int l = 0;
         int r = nums.length;
+        
         while(l < r){
             int m = l + (r - l)/2;
-            if(nums[m] == target) return m;
-            else if(nums[m] > target){
+            if(nums[m] >= target){
                 r = m;
             }else{
                 l = m + 1;
