a2ee299b6a2fc743b43e3647ae65d5624c06da44
jiajionline <jiajionline@gmail.com>
2020-03-25 19:50:36 -0700

    Leetcode 136 Single Number

---
+++ 0136_Single Number/SingleNumber.cs
@@ -0,0 +1,11 @@
+﻿class Solution {
+    public int SingleNumber (int[] nums) {
+        int ret = 0;
+
+        foreach (var i in nums) {
+            ret ^= i;
+        }
+
+        return ret;
+    }
+}
---
+++ 0136_Single Number/SingleNumber.java
@@ -0,0 +1,10 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int ans = 0;
+        for(int v : nums){
+            ans ^= v;
+        }
+        
+        return ans;
+    }
+}
