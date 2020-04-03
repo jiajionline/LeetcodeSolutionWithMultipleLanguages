e9805e340a50ba2b4f76490b12ae0bffd4a057dd
jiajionline <jiajionline@gmail.com>
2020-04-03 06:43:05 -0700

    Leetcode 645 Set Mismatch

---
+++ 0645_Set Mismatch/SetMismatch.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int[] FindErrorNums(int[] nums)
+    {
+        int repition = int.MinValue;
+        int missing = int.MinValue;
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[Math.Abs(nums[i]) - 1] < 0)
+            {
+                repition = Math.Abs(nums[i]);
+            }else
+            {
+                nums[Math.Abs(nums[i]) - 1] *= -1;
+            }
+        }
+
+        for(int i = 0; i < nums.Length; i++)
+        {
+            if(nums[i] > 0)
+            {
+                missing = i+1;
+            }
+        }
+
+        return new int[] { repition, missing };
+    }
+}
+
