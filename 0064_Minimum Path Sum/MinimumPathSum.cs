0e646232c59addebefba50e3bceaef8380f25b88
jiajionline <jiajionline@gmail.com>
2020-04-04 21:55:37 -0700

    Leetcode 64 Minimum Path Sum

---
+++ 0064_Minimum Path Sum/MinimumPathSum.cs
@@ -0,0 +1,19 @@
+﻿public class Solution {
+    public int MinPathSum(int[][] grid) {
+        if(grid == null || grid.Length == 0) return 0;
+        for(int i=0;i<grid.Length;i++){
+            for(int j=0;j<grid[i].Length;j++){
+                if(i == 0 && j == 0) continue;
+                if(i == 0){
+                    grid[0][j] += grid[0][j-1];
+                }else if(j == 0){
+                    grid[i][0] += grid[i-1][0];
+                }else{
+                    grid[i][j] = Math.Min(grid[i-1][j], grid[i][j-1]) + grid[i][j];
+                }
+            }
+        }
+        
+        return grid[grid.Length-1][grid[0].Length-1];
+    }
+}
