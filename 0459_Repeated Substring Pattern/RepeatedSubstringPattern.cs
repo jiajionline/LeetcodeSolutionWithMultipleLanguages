8f43059ec46679cd40be5c2805fb7cee718eb9e5
jiajionline <jiajionline@gmail.com>
2020-04-05 19:45:55 -0700

    Leetcode 459 Repeated Substring Pattern

---
+++ 0459_Repeated Substring Pattern/RepeatedSubstringPattern.cs
@@ -0,0 +1,24 @@
+﻿public class Solution {
+   public bool RepeatedSubstringPattern(string s) {
+       if(string.IsNullOrEmpty(s)) return false;
+       var l = s.Length;
+       if(l == 1) return false;
+       for(int i=1;i<=l/2;i++)
+       {
+           if(l % i == 0){
+               int subCount = (l / i);
+               var subStr = s.Substring(0, l/subCount);
+               var sb = new StringBuilder();
+              
+               for(int j=1;j<=subCount;j++){
+                   sb.Append(subStr);
+               }
+
+               if(string.Equals(sb.ToString(), s)){
+                   return true;
+               }
+           }
+       }    
+       return false; 
+   }
+}
