4315600e51e7ff028c5ac23f4f0ba1bbb1829f2f
jiajionline <jiajionline@gmail.com>
2020-05-01 16:47:57 -0700

    Leetcode Top K Frequent Words

---
+++ 0692_Top K Frequent Words/TopKFrequentWords.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<String> topKFrequent(String[] words, int k) {
+        
+        List<String> result = new LinkedList<>();
+        Map<String, Integer> map = new HashMap<>();
+        for(int i=0; i<words.length; i++)
+        {
+            if(map.containsKey(words[i]))
+                map.put(words[i], map.get(words[i])+1);
+            else
+                map.put(words[i], 1);
+        }
+        
+        PriorityQueue<Map.Entry<String, Integer>> pq = new PriorityQueue<>(
+                 (a,b) -> a.getValue()==b.getValue() ? b.getKey().compareTo(a.getKey()) : a.getValue()-b.getValue()
+        );
+        
+        for(Map.Entry<String, Integer> entry: map.entrySet())
+        {
+            pq.offer(entry);
+            if(pq.size()>k)
+                pq.poll();
+        }
+
+        while(!pq.isEmpty())
+            result.add(0, pq.poll().getKey());
+        
+        return result;
+    }
+}
