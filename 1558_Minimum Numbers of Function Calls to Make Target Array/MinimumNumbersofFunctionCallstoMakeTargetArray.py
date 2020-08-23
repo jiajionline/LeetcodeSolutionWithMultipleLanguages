b9e66bfd1e46115cdea7e0d0f4478fe7722543d0
Jiajionline <Jiajionline@gmail.com>
2020-08-23 07:08:36 -0700

    Leetcode 1558 Minimum Numbers of Function Calls to Make Target Array

--- 1558_Minimum Numbers of Function Calls to Make Target Array/MinimumNumbersofFunctionCallstoMakeTargetArray.py
+++ 1558_Minimum Numbers of Function Calls to Make Target Array/MinimumNumbersofFunctionCallstoMakeTargetArray.py
@@ -1,3 +1,5 @@
 class Solution:
     def minOperations(self, nums: List[int]) -> int:
+        # first part is counting 1s , second part is finding the max highest 1 but need to -3 since in python, 
+        # binary starts '0b', also need to minus extra 1.
         return sum(bin(x).count('1') for x in nums) + len(bin(max(nums))) - 3
