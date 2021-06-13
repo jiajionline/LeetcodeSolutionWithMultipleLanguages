3a6b2cc7f1f7b6626f4d1d430f9a7505d604cd5d
Jiajionline <Jiajionline@gmail.com>
2021-06-13 06:25:49 -0700

    Leetcode 20 Valid Parentheses solution for javascript

---
+++ 0020_Valid Parentheses/ValidParentheses.js
@@ -0,0 +1,28 @@
+﻿/**
+ * @param {string} s
+ * @return {boolean}
+ */
+ var isValid = function(s){
+    
+    if(s.length % 2 == 1) return false;
+    
+    const stack = []
+
+    for (i=0; i < s.length; i++){
+
+        let curr = s[i];
+        
+        switch(curr) {
+            case '(': stack.push(')');
+                break;
+            case '[': stack.push(']');
+                break;
+            case '{': stack.push('}')
+                break;
+            default:
+                elem = stack.pop()
+                if (curr !== elem) return false;       
+        }
+    }
+    return stack.length === 0;
+}
