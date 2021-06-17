c9a42daa401dd079991a6359e2bba242e73814cd
Jiajionline <Jiajionline@gmail.com>
2021-06-17 15:09:13 -0700

    Leetcode 142 Linked List Cycle II

---
+++ 0142_Linked List Cycle II/LinkedListCycleII.cs
@@ -0,0 +1,34 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode DetectCycle(ListNode head) {
+        if(head == null) return null;
+        var fast = head;
+        var slow = head;
+        
+        while(fast!=null && fast.next!=null){
+            fast = fast.next.next;
+            slow = slow.next;
+            if(fast == slow) break;
+        }
+        
+        if(fast == null || fast.next == null) return null;
+        
+        var p = head;
+        while(p != slow){
+            p = p.next;
+            slow = slow.next;
+        }
+        
+        return p;
+    }
+}
