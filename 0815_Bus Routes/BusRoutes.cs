742ba44f41d883c23e192c200fd39813d868975f
Jiajionline <Jiajionline@gmail.com>
2021-06-18 13:15:53 -0700

    Leetcode 815 Bus Routes

--- 0815_Bus Routes/BusRoutes.cs
+++ 0815_Bus Routes/BusRoutes.cs
@@ -1,36 +1,48 @@
 public class Solution {
-    public int NumBusesToDestination(int[][] routes, int S, int T) {
-        if(S == T) return 0;
-        //build a graph
-        var dict = new Dictionary<int, List<int>>();
+    public int NumBusesToDestination(int[][] routes, int source, int target) {
+        if(routes == null || routes.Length == 0) return -1;
+        if(source == target) return 0;
+        var graph = new Dictionary<int, List<int>>();
         for(int i=0;i<routes.Length;i++){
-            foreach(var stop in routes[i]){
-                if(!dict.ContainsKey(stop)) dict.Add(stop, new List<int>());
-                dict[stop].Add(i);
+            var stops = routes[i];
+            foreach(var s in stops){                
+                if(!graph.ContainsKey(s))    
+                    graph.Add(s, new List<int>());
+                graph[s].Add(i);
             }
         }
-
-        var visited = new bool[501];
+        // key is stop, value is route
+        var dict = new Dictionary<int, int>();
+        // check if the route processed or not
+        var visited = new HashSet<int>();
+        
         var queue = new Queue<int>();
-        queue.Enqueue(S);
-        int buses = 0;
-        int count = queue.Count;
-        while(queue.Count > 0){
-            int size = queue.Count;
+
+        queue.Enqueue(source);
+        var buses = 0;
+        
+        while(queue.Count > 0)
+        {
             buses++;
-            while(size-- > 0){
-                var current = queue.Dequeue();                
-                foreach(var bus in dict[current]){
-                    if(visited[bus]) continue;
-                    visited[bus] = true;
-                    foreach(var stop in routes[bus]){
-                        if(stop == T) return buses;
+            var size = queue.Count;
+            
+            while(size-- > 0)
+            {
+                var currStop = queue.Dequeue();
+                
+                foreach(var route in graph[currStop])
+                {
+                    if(visited.Contains(route)) continue;
+                    visited.Add(route);
+                    foreach(var stop in routes[route]){
+                        if(stop == target) return buses;  
                         queue.Enqueue(stop);
                     }
                 }
             }
         }
-
+        
         return -1;
+        
     }
 }
