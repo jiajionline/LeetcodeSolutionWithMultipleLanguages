613e403cf9a1d9fb3a3a369393b5f2d415878b42
Jiajionline <Jiajionline@gmail.com>
2021-06-20 06:16:14 -0700

    Leetcode 112 Path Sum

--- 0112_Path Sum/PathSum.cs
+++ 0112_Path Sum/PathSum.cs
@@ -1,31 +1,35 @@
-﻿public class Solution {
-   public bool HasPathSum(TreeNode root, int sum) {
-       if(root == null) return false;
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
         var queue = new Queue<Tuple<TreeNode,int>>();
-        queue.Enqueue(new Tuple<TreeNode,int>(root, root.val));
-
-        while(queue.Count()>0)
+        queue.Enqueue(new Tuple<TreeNode,int>(root, targetSum));
+        
+        while(queue.Count > 0)
         {
-            var tmp = queue.Dequeue();
-            var tmpNode = tmp.Item1;
-            var tmpValue = tmp.Item2;
-            if(tmpNode.left == null && tmpNode.right == null)
-            {
-                if(tmpValue == sum)
-                    return true;
-            }
-
-            if(tmpNode.left!=null)
-            {
-                queue.Enqueue(new Tuple<TreeNode,int>(tmpNode.left, tmpValue + tmpNode.left.val));
-            }
-
-            if(tmpNode.right!=null)
-            {
-                queue.Enqueue(new Tuple<TreeNode,int>(tmpNode.right, tmpValue + tmpNode.right.val));
-            }
+            var v = queue.Dequeue();
+            var node = v.Item1;
+            var sum = v.Item2;
+            if(node.left == null && node.right == null && sum == node.val) return true;
+            if(node.left != null)
+                queue.Enqueue(new Tuple<TreeNode, int>(node.left, sum - node.val));
+            
+            if(node.right != null)
+                queue.Enqueue(new Tuple<TreeNode, int>(node.right, sum - node.val));
         }
-      
-       return false;
-   }
+        
+        return false;
+    }
 }
---
+++ 0112_Path Sum/PathSum2.cs
@@ -0,0 +1,21 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public bool HasPathSum(TreeNode root, int targetSum) {
+        if(root == null) return false;
+        if(root.left == null && root.right == null && targetSum == root.val) return true;
+        return HasPathSum(root.left, targetSum - root.val) || HasPathSum(root.right, targetSum - root.val);
+    }
+    
+}
