ada99b66d99ba3811c6a910d64cd26982907c182
Jiajionline <Jiajionline@gmail.com>
2021-06-29 19:56:24 -0700

    Leetcode 617 Merge Two Binary Trees

---
+++ 0617_Merge Two Binary Trees/MergeTwoBinaryTrees2.cs
@@ -0,0 +1,25 @@
+﻿/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode MergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return null;
+        if(root1 == null) return root2;
+        if(root2 == null) return root1;
+        root1.val += root2.val;
+        root1.left = MergeTrees(root1.left, root2.left);
+        root1.right = MergeTrees(root1.right, root2.right);
+        
+        return root1;
+    }
+}
