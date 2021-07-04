004e1f7eb4461239ac432f8909dcb346a05e084f
Jiajionline <Jiajionline@gmail.com>
2021-07-04 07:12:56 -0700

    Leetcode 669 Trim a Binary Search Tree

--- 0669_Trim a Binary Search Tree/TrimaBinarySearchTree2.cs
+++ 0669_Trim a Binary Search Tree/TrimaBinarySearchTree2.cs
@@ -16,20 +16,14 @@ public class Solution {
         if(root == null) return null;
         var left = TrimBST(root.left, low, high);
         var right = TrimBST(root.right, low, high);
-        
         root.left = left;
         root.right = right;
         
-        if((root.val < low || root.val > high)){
-           if(root.left == null && root.right == null){
-               return null;
-           }else if(root.left !=null){
-               return root.left;
-           }else{
-               return root.right;
-           }
-        }else{
+        if(root.val < low)
+            return right;
+        else if(root.val > high) 
+            return left;
+        else
             return root;
-        }
     }
 }
