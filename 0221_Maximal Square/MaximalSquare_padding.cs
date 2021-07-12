558b157c1f557c25d49c6b59df413ef9a2c5da20
Jiajionline <Jiajionline@gmail.com>
2021-07-12 07:35:00 -0700

    Leetcode 221 Maximal Square for c# solution

---
+++ 0221_Maximal Square/MaximalSquare_padding.cs
@@ -0,0 +1,25 @@
+public class Solution {
+    public int MaximalSquare(char[][] matrix) {
+        if(matrix == null || matrix.Length == 0 || matrix[0].Length == 0) return 0;
+        var m = matrix.Length;
+        var n = matrix[0].Length;
+        
+        var ans = 0;
+        
+        var dp = new int[m+1,n+1];
+        
+        for(int i=1;i<=m;i++){
+            for(int j=1;j<=n;j++){
+                if(matrix[i-1][j-1] == '0'){
+                    dp[i,j] = 0;
+                }else{
+                    dp[i,j] = Math.Min(dp[i-1,j], Math.Min(dp[i,j-1], dp[i-1,j-1])) + 1;
+                }
+                
+                ans = Math.Max(ans, dp[i,j]);
+            }
+        }
+        
+        return ans * ans;
+    }
+}
