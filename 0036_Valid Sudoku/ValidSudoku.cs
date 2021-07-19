41e23592bbe9abc0ef6e895566e7c5f517028d47
Jiajionline <Jiajionline@gmail.com>
2021-07-19 09:44:20 -0700

    Leetcode 36 Valid Sudoku update

--- 0036_Valid Sudoku/ValidSudoku.cs
+++ 0036_Valid Sudoku/ValidSudoku.cs
@@ -1,27 +1,29 @@
 public class Solution {
-   public bool IsValidSudoku(char[][] board)
-   {
-       if (board == null) return false;
-       int row = board.Length;
-       int column = board[0].Length;
-
-       if (row != 9 || column != 9) return false;
-
-       var seen = new HashSet<string>();
-       for (int i = 0; i < 9; ++i)
-       {
-           for (int j = 0; j < 9; ++j)
-           {
-               char number = board[i][j];
-               if(number != '.' && (number - '0'< 0 || number - '9' > 9)) return false;
-               if(number == '.') continue;
-              
-               if (!seen.Add(number + " in row " + i) ||
-                   !seen.Add(number + " in column " + j) ||
-                   !seen.Add(number + " in block " + i / 3 + "-" + j / 3))
-                       return false;
-           }
-       }
-       return true;        
-   }
+    public bool IsValidSudoku(char[][] board) {
+        if(board == null) return false;
+        var rows = board.Length;
+        var cols = board[0].Length;
+        
+        if(rows != 9 || cols != 9) return false;
+        
+        var set = new HashSet<string>();
+        
+        for(int row = 0;row < rows;row++)
+        {
+            for(int col = 0;col < cols; col++)
+            {
+                var c = board[row][col];
+                if(c == '.') continue;
+                if(c - '0' < 1 || c - '0' > 9) return false;
+                if( !set.Add($"{c} in rows {row}") || 
+                    !set.Add($"{c} in cols {col}") || 
+                    !set.Add($"{c} in block {row/3} and {col/3}")){
+                        return false;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
 }
