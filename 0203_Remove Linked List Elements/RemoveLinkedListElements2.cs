15a6e53db57863caa252548d1c345976032b1e05
Jiajionline <Jiajionline@gmail.com>
2021-09-20 07:44:16 -0700

    Leetcode 203 Remove Linked List Elements

---
+++ 0203_Remove Linked List Elements/RemoveLinkedListElements2.cs
@@ -0,0 +1,31 @@
+﻿/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     public int val;
+ *     public ListNode next;
+ *     public ListNode(int val=0, ListNode next=null) {
+ *         this.val = val;
+ *         this.next = next;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode RemoveElements(ListNode head, int val) {
+        if(head == null) return null;
+        var prehead = new ListNode(-1, head);
+        var p = prehead;
+        
+        while(p!=null && p.next!=null)
+        {
+            if(p.next.val == val)
+            {
+                p.next = p.next.next;
+            }else if(p.next.val != val)
+            {
+                p = p.next;
+            }
+        }
+        
+        return prehead.next;
+    }
+}
