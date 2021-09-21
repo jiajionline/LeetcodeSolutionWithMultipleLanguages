4099825e2803840d26a3d4b2d530c1c2bf554264
Jiajionline <Jiajionline@gmail.com>
2021-09-21 06:29:47 -0700

    Leetcode 242 Valid Anagram

---
+++ 0242_Valid Anagram/ValidAnagram2.cs
@@ -0,0 +1,20 @@
+﻿public class Solution {
+    public bool IsAnagram(string s, string t) {
+        if(string.IsNullOrEmpty(s) && string.IsNullOrEmpty(t)) return true;
+        if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(t)) return false;
+        if(s.Length != t.Length) return false;
+        
+        var count = new int[26];
+        foreach(var c in s)
+        {
+            count[c-'a']++;
+        }
+        
+        foreach(var c in t)
+        {
+            count[c-'a']--;
+        }
+        
+        return !(count.Where(t=> t!=0).Count() > 0);
+    }
+}
