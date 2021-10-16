a10bb8b35e4ddcd1e2218ad4b13328d2cb14f202
Jiajionline <Jiajionline@gmail.com>
2021-10-15 18:04:41 -0700

    Leetcode 547 Number of Provinces for c# solution

--- 0547_Number of Provinces/NumberofProvinces.cs
+++ 0547_Number of Provinces/NumberofProvinces.cs
@@ -1,25 +1,32 @@
-﻿public class Solution {
-    public int FindCircleNum(int[][] isConnected) {
-        if(isConnected == null) return 0;
+﻿public class Solution
+{
+    public int FindCircleNum(int[][] isConnected)
+    {
         var ans = 0;
+        if (isConnected == null) return ans;
+
         var visited = new bool[isConnected.Length];
-        
-        for(int i=0;i<isConnected.Length;i++){
-            if(visited[i] == false){
-                DFS(isConnected,i,visited);
+        for (int i = 0; i < isConnected.Length; i++)
+        {
+            if (!visited[i])
+            {
                 ans++;
+                DFS(isConnected, visited, i);
             }
         }
-        
+
         return ans;
     }
-    
-    private void DFS(int[][] isConnected, int current, bool[] visited)
+
+    private void DFS(int[][] isConnected, bool[] visited, int i)
     {
-        for(int i=0;i<isConnected[current].Length;i++){
-            if(isConnected[current][i] == 1 && visited[i] == false){
-                visited[i] = true;
-                DFS(isConnected,i,visited);
+        if (visited[i]) return;
+        visited[i] = true;
+        for (int j = 0; j < isConnected[i].Length; j++)
+        {
+            if (isConnected[i][j] == 1 && !visited[j])
+            {
+                DFS(isConnected, visited, j);
             }
         }
     }
