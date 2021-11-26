85a33ef230cab8b494b84dded74c50fc58f232a4
Jiajionline <Jiajionline@gmail.com>
2021-11-26 14:21:44 -0800

    Leetcode 79 Word Search

--- 0079_Word Search/WordSearch.cs
+++ 0079_Word Search/WordSearch.cs
@@ -2,7 +2,7 @@ public class Solution {
     public bool Exist(char[][] board, string word) {
         if(board == null) return false;
         for(int i=0;i<board.Length;i++){
-            for(int j=0;j<board[0].Length;j++){
+            for(int j=0;j<board[i].Length;j++){
                 if(Search(board,word,i,j,0)) return true;        
             }
         }
@@ -11,10 +11,9 @@ public class Solution {
     }
     
     private bool Search(char[][] board,string word, int x,int y, int p){
-        if(x<0 || y<0 || x>=board.Length || y>=board[0].Length) return false;
+        if(x<0 || y<0 || x==board.Length || y==board[x].Length || board[x][y] != word[p]) return false;
         //not necessary using this,since the next line always returns false if board[x][y] is '#'
         //if(board[x][y] == '#') return false;
-        if(board[x][y] != word[p]) return false;
         if(p == word.Length-1) return true;
         char original = board[x][y];
         board[x][y] = '#';
