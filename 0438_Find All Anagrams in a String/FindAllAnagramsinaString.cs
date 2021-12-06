05bc214aeaea578dfdab62d557b48267df887251
Jiajionline <Jiajionline@gmail.com>
2021-12-06 06:12:48 -0800

    Leetcode 438 Find All Anagrams in a String

--- 0438_Find All Anagrams in a String/FindAllAnagramsinaString.cs
+++ 0438_Find All Anagrams in a String/FindAllAnagramsinaString.cs
@@ -1,42 +1,32 @@
 public class Solution {
-   public IList<int> FindAnagrams(string s, string p) {
-       var ret = new List<int>();
-       if(string.IsNullOrEmpty(s) || string.IsNullOrEmpty(p)) return ret;
-       if(s.Length < p.Length) return ret;
-
-       int[] arr = new int[26];
-
-       for(int i=0;i<p.Length;i++)
-       {
-           arr[p[i] - 'a']++;
-       }
-
-       for(int i=0;i<p.Length;i++)
-       {
-           arr[s[i] - 'a']--;
-       }
-
-       if(IsAnagrams(arr)){
-           ret.Add(0);
-       }
-
-       for(int i=p.Length;i<s.Length;i++)
-       {
-           var begin = i - p.Length;
-           arr[s[begin] - 'a']++;
-           arr[s[i] - 'a']--;
-           if(IsAnagrams(arr)) ret.Add(begin+1);
-       }
-
-       return ret;
-   }
-
-   private bool IsAnagrams(int[] arr)
-   {
-       foreach(var i in arr){
-           if(i!=0) return false;
-       }
-
-       return true;
-   }
+    public IList<int> FindAnagrams(string s, string p) {
+        var ans = new List<int>();
+        var l_p = p.Length;
+        var l_s = s.Length;
+        
+        if(l_s < l_p) return ans;
+        var count = new int[26];
+        for(int i=0;i<l_p;i++)
+        {
+            count[p[i]-'a']++;
+            count[s[i]-'a']--;
+        }
+        
+        if(AllZeros(count)) ans.Add(0);
+        
+        for(int i=l_p;i<l_s;i++)
+        {
+            count[s[i]-'a']--; 
+            count[s[i-l_p]-'a']++;
+            if(AllZeros(count))
+                ans.Add(i-l_p+1);
+        }
+        
+        return ans;
+    }
+    
+    private bool AllZeros(int[] count)
+    {
+        return count.Where(c => c!=0).Count() == 0;
+    }
 }
