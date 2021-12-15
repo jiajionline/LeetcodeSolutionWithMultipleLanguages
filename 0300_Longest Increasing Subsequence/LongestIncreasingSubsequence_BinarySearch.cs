a7855c01ba08ae541791215594cc18a359a2187a
Jiajionline <Jiajionline@gmail.com>
2021-12-15 09:22:25 -0800

    Leetcode 300 Longest Increasing Subsequence for Binary Search solution

---
+++ 0300_Longest Increasing Subsequence/LongestIncreasingSubsequence_BinarySearch.cs
@@ -0,0 +1,32 @@
+public class Solution {
+    public int LengthOfLIS(int[] nums) {
+        if(nums.Length == 1) return 1;
+        var dp = new List<int>();
+        for(int i=0;i<nums.Length;i++)
+        {
+            var pos = BinarySearch(dp,nums[i]);
+            if(pos == dp.Count)
+                dp.Add(nums[i]);
+            else
+                dp[pos] = nums[i];
+        }
+        
+        return dp.Count();
+    }
+    
+    private int BinarySearch(List<int> list, int target)
+    {
+        var l = 0;
+        var r = list.Count - 1;
+        while(l <= r)
+        {
+            var mid = (r-l)/2 + l;
+            if(list[mid] >= target)
+                r = mid-1;
+            else
+                l = mid+1;
+        }
+        
+        return l;
+    }
+}
