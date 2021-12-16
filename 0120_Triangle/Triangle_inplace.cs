510a9cdd6dfb25923d2badb71439ddd9c36fb42f
Jiajionline <Jiajionline@gmail.com>
2021-12-16 05:10:55 -0800

    Leetcode 120 Triangle

--- 0120_Triangle/Triangle_inplace.cs
+++ 0120_Triangle/Triangle_inplace.cs
@@ -3,16 +3,15 @@ public class Solution {
         var n = triangle.Count;
         if(n == 1) return triangle[0][0];
         
-        var dp = new int[n+1];
-        
-        for(int i=n-1;i>=0;i--)
+        for(int i=n-2;i>=0;i--)
         {
             var list = triangle[i];
             for(int j=0;j<list.Count;j++)
-                dp[j] = Math.Min(dp[j], dp[j+1]) + list[j];
-            
+            {
+                list[j] += Math.Min(triangle[i+1][j], triangle[i+1][j+1]);
+            }
         }
         
-        return dp[0];
+        return triangle[0][0];
     }
 }
