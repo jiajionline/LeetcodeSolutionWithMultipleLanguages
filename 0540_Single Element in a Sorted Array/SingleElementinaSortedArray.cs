c5c04725c60d2728feaa13080cec4c651d70c574
Jiajionline <Jiajionline@gmail.com>
2021-12-22 14:17:36 -0800

    Leetcode 540 Single Element in a Sorted Array

--- 0540_Single Element in a Sorted Array/SingleElementinaSortedArray.cs
+++ 0540_Single Element in a Sorted Array/SingleElementinaSortedArray.cs
@@ -2,18 +2,18 @@ public class Solution {
     public int SingleNonDuplicate(int[] nums) {
         
         var l = 0;
-        var r = nums.Length-1;
+        var r = nums.Length;
         
-        while(l <= r)
+        while(l < r)
         {
             var m = (r - l)/2 + l;
+            if(m <= 0 || m+1 >= nums.Length) return nums[m];
             var n = (m % 2 == 0 ? m+1 : m-1);
-            if(n >= nums.Length || n < 0) return nums[m];
             if(nums[m] == nums[n])
             {
                 l = m+1;
             }else{
-                r = m-1;
+                r = m;
             }
         }
         
