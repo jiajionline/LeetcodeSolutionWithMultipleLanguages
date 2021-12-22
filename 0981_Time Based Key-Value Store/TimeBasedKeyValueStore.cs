5fd0b2f07d16ba10fb63bfcd77be7bf8f421ccc7
Jiajionline <Jiajionline@gmail.com>
2021-12-22 09:15:09 -0800

    Leetcode 981 Time Based Key-Value Store

--- 0981_Time Based Key-Value Store/TimeBasedKeyValueStore.cs
+++ 0981_Time Based Key-Value Store/TimeBasedKeyValueStore.cs
@@ -1,42 +1,42 @@
 public class TimeMap {
-
-    private Dictionary<string, List<Tuple<string,int>>> dict = new Dictionary<string, List<Tuple<string,int>>>();
-    /** Initialize your data structure here. */
+    private Dictionary<string, List<Tuple<int,string>>> dict;
     public TimeMap() {
-        
+        dict = new Dictionary<string, List<Tuple<int, string>>>();
     }
     
     public void Set(string key, string value, int timestamp) {
-        if(!dict.ContainsKey(key)) dict.Add(key, new List<Tuple<string,int>>());
-        dict[key].Add(Tuple.Create(value, timestamp));
+        if(!dict.ContainsKey(key))
+        {
+            var list = new List<Tuple<int, string>>();
+            list.Add(Tuple.Create(timestamp, value));
+            dict.Add(key, list);
+        }
+        else
+        {
+            dict[key].Add(Tuple.Create(timestamp, value));    
+        }
+            
     }
     
     public string Get(string key, int timestamp) {
-        if(dict.ContainsKey(key))
+        if(!dict.ContainsKey(key)) return "";
+        var list = dict[key];
+        var l = 0;
+        var r = list.Count;
+        
+        while(l < r)
         {
-            var list = dict[key];
-            var l = 0;
-            var r = list.Count-1;
-            
-            while(l <= r)
-            {
-                var mid = (r - l)/2 + l;
-                if(list[mid].Item2 > timestamp)
-                {
-                    r = mid - 1;
-                }
-                else
-                {
-                    l = mid + 1;
-                }
-            }
-            
-            if(l-1 < 0) return "";
-            if(l-1 >= list.Count) return "";
-            return list[l-1].Item1;
+            var mid = (r-l)/2 + l;
+            if(list[mid].Item1 > timestamp)
+                r = mid;
+            else
+                l = mid+1;
         }
         
-        return "";
+        if (l-1 >= 0 && l-1 < list.Count) 
+            return list[l-1].Item2;
+        else
+            return "";
     }
 }
 
