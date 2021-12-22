d5a4c4567c026b692c147a7dbb81a949c2a1ecc0
Jiajionline <Jiajionline@gmail.com>
2021-12-22 09:16:23 -0800

    Leetcode 287 Find the Duplicate Number

--- 0287_Find the Duplicate Number/FindtheDuplicateNumber_BinarySearch.java
+++ 0287_Find the Duplicate Number/FindtheDuplicateNumber_BinarySearch.java
@@ -1,11 +1,11 @@
 ﻿class Solution {
     
     public int findDuplicate(int[] nums) {      
-        int l = 1, r = nums.length - 1;
+        int l = 1, r = nums.length;
         
-        while (l <= r) {
-            int mid = (l + r) / 2;
-
+        while (l < r) {
+            int mid = (r - l) / 2 + l;
+ 
             int count = 0;
             for (int num : nums) {
                 if (num <= mid)
@@ -15,7 +15,7 @@
             // Normally, if there is no duplicate before mid, the count should be equals to mid
             // if count > mid, means there is duplicate before mid, should continue to search [l, mid-1]
             if (count > mid) {
-                r = mid - 1;
+                r = mid;
             } else {
                 l = mid + 1;
             }
