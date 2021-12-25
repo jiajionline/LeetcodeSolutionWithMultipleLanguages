7281b3b500e2fec877e64043453b8e21dd31c773
Jiajionline <Jiajionline@gmail.com>
2021-12-25 13:47:50 -0800

    Leetcode 96 Unique Binary Search Trees

--- 0096_Unique Binary Search Trees/UniqueBinarySearchTrees.cs
+++ 0096_Unique Binary Search Trees/UniqueBinarySearchTrees.cs
@@ -1,24 +1,29 @@
 public class Solution {
-    public int NumTrees (int n) {
-        var dict = new Dictionary<int, int> () { { 0, 1 }, { 1, 1 } };
-        return Calc (n, dict);
+       
+    public int NumTrees(int n) {
+        var memo = new int[n+1];
+        return NumTrees(n, memo);
     }
-
-    private int Calc (int n, Dictionary<int, int> dict) {
-        if (dict.ContainsKey (n)) {
-            return dict[n];
-        } else {
-            int sum = 0;
-            for (int root = 1; root <= n; root++) {
-                var left = Calc (root - 1, dict);
-                var right = Calc (n - root, dict);
-                sum += left * right;
-            }
-
-            if (!dict.ContainsKey (n))
-                dict.Add (n, sum);
-
-            return sum;
+    
+    private int NumTrees(int n, int[] memo)
+    {
+        if(n <= 1) return 1;
+        if(memo[n] > 0) return memo[n];
+        
+        var sum = 0;
+        // the node as a root is from to n inclusively
+        for(int root=1;root<=n;root++)
+        {
+            //calculate the left nodes of root 
+            var left = NumTrees(root-1, memo);
+            //calculate the right nodes of root
+            //make sure the total left + right should be equal to n - 1 (root - 1 + n - root == n-1)
+            var right = NumTrees(n - root, memo);
+            sum += left * right;
+            
         }
+        
+        memo[n] = sum;
+        return sum;
     }
 }
