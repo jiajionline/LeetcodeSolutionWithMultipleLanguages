75048a45980ee59f95812f987244485521d9737f
Jiajionline <Jiajionline@gmail.com>
2021-12-28 05:41:56 -0800

    Leetocde 476 Number Complement

---
+++ 0476_Number Complement/NumberComplement_2.cs
@@ -0,0 +1,14 @@
+﻿public class Solution {
+    public int FindComplement(int num) {
+        var copy = num;
+        var k = 1;
+        while(copy > 0)
+        {
+            num ^= k;
+            k <<= 1;
+            copy >>= 1;
+        }
+        
+        return num;
+    }
+}
---
+++ 0476_Number Complement/NumberComplement_3.cs
@@ -0,0 +1,7 @@
+﻿public class Solution {
+    public int FindComplement(int num) {
+        var mask = ~0;
+        while((num & mask) > 0) mask <<= 1;
+        return ~num ^ mask;
+    }
+}
