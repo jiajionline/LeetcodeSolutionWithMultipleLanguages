d31b5ac576bbb192c53928c047b76ad939a954b2
Jiajionline <Jiajionline@gmail.com>
2022-01-03 06:26:13 -0800

    Leecode 10 Regular Expression Matching

---
+++ 0010_Regular Expression Matching/RegularExpressionMatching_Recursion.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isMatch(String s, String p) {
+        if(p.isEmpty()) return s.isEmpty();
+        boolean firstMatch = !s.isEmpty() && (s.charAt(0) == p.charAt(0) || p.charAt(0) == '.');
+        
+        if(p.length() >=2 && p.charAt(1) == '*') {
+            return isMatch(s, p.substring(2)) || (firstMatch && isMatch(s.substring(1), p));
+        }else {
+            return firstMatch && isMatch(s.substring(1), p.substring(1));
+        }
+    }
+}
