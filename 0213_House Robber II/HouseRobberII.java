039721c5602e1b6e5e6e9edc4a7d19cd2f3715bf
Jiajionline <Jiajionline@gmail.com>
2022-01-06 05:53:49 -0800

    Leetcode 213 House Robber II

--- 0213_House Robber II/HouseRobberII.java
+++ 0213_House Robber II/HouseRobberII.java
@@ -1,21 +1,23 @@
 class Solution {
     public int rob(int[] nums) {
         int n = nums.length;
-        if(n == 0) return 0;
         if(n == 1) return nums[0];
-        if(n == 2) return Math.max(nums[0],nums[1]);
+        if(n == 2) return Math.max(nums[0], nums[1]);
         
-        return Math.max(rob(nums, nums[0], nums[0], 2,n-1), rob(nums, 0, nums[1],2,n));
-       
+        return Math.max(rob(nums,0,n-2), rob(nums,1,n-1));
+        
     }
-
-    private int rob(int[] nums, int previous, int current, int start, int end){
-        for(int i=start;i<end;i++){
-            int next = Math.max(current, previous + nums[i]);
-            previous = current;
-            current = next;
-        }       
-
-        return current;
+    
+    private int rob(int[] nums, int from, int to) {
+        int prev2 = nums[from];
+        int prev1 = Math.max(prev2, nums[from+1]);
+        
+        for(int i=from+2;i<=to;i++) {
+            int curr = Math.max(prev2 + nums[i], prev1);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+        
+        return prev1;
     }
 }
