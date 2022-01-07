84ff7426c48792d9d46180df1b72ef157eb0e333
Jiajionline <Jiajionline@gmail.com>
2022-01-06 16:42:02 -0800

    Leetcode 167 Two Sum II

---
+++ 0167_Two Sum II/TwoSumII.java
@@ -0,0 +1,21 @@
+﻿class Solution {
+    public int[] twoSum(int[] numbers, int target) {
+        int[] ans = new int[2];
+        int n = numbers.length;
+        int l = 0, r = n-1;
+        
+        while(l < r) {
+            if(numbers[l] + numbers[r] == target) {
+                ans[0] = l+1;
+                ans[1] = r+1;
+                break;
+            }else if(numbers[l] + numbers[r] > target) {
+                r--;
+            }else {
+                l++;
+            }
+        }
+        
+        return ans;
+    }
+}
