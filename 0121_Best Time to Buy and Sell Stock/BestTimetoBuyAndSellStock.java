abfb6f59d7e44406d5abb94882e42d4596bf40ad
Jiajionline <Jiajionline@gmail.com>
2022-01-06 16:49:04 -0800

    Leetcode 121 Best Time to Buy and Sell Stock

---
+++ 0121_Best Time to Buy and Sell Stock/BestTimetoBuyAndSellStock.java
@@ -0,0 +1,13 @@
+﻿class Solution {
+    public int maxProfit(int[] prices) {
+        if(prices.length <= 1) return 0;
+        int currMin = prices[0];
+        int ans = 0;
+        for(int i=1;i<prices.length;i++) {
+            ans = Math.max(ans, prices[i] - currMin);
+            currMin = Math.min(currMin, prices[i]);
+        }
+        
+        return ans;
+    }
+}
