bbed4832c8b9dbda06ea2626b2da6c3d8491aacf
Jiajionline <Jiajionline@gmail.com>
2022-01-09 07:48:55 -0800

    Leetcode 206 Reverse Linked List for Java solution

---
+++ 0206_Reverse Linked List/ReverseLinkedList.java
@@ -0,0 +1,17 @@
+﻿class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head == null || head.next == null) return head;
+        ListNode prev = null;
+        ListNode curr = head;
+        ListNode next = null;
+        
+        while(curr!=null) {
+            next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        
+        return prev;
+    }
+}
---
+++ 0206_Reverse Linked List/ReverseLinkedList2.java
@@ -0,0 +1,13 @@
+﻿class Solution {
+    public ListNode reverseList(ListNode head) {
+        return reverseList(head, null);
+    }
+    
+    private ListNode reverseList(ListNode curr, ListNode prev)
+    {
+        if(curr == null) return prev;
+        ListNode next = curr.next;
+        curr.next = prev;
+        return reverseList(next, curr);
+    }
+}
