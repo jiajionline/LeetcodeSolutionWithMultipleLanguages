aa71ac67d1805f35906e8658d6d2326c974c9ba4
Jiajionline <Jiajionline@gmail.com>
2022-01-09 17:44:22 -0800

    Leetcode 3 Longest Substring Without Repeating Characters

---
+++ 0003_Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters_1.cs
@@ -0,0 +1,22 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var arr = new int[128];
+        var l = 0;
+        var r = 0;
+        var ans = 0;
+        while(r < s.Length)
+        {
+            arr[s[r]]++;
+            
+            while(arr[s[r]] > 1)
+            {
+                arr[s[l++]]--;
+            }
+            
+            ans = Math.Max(ans, r - l + 1);
+            r++;
+        }
+        
+        return ans;
+    }
+}
---
+++ 0003_Longest Substring Without Repeating Characters/LongestSubstringWithoutRepeatingCharacters_2.cs
@@ -0,0 +1,26 @@
+﻿public class Solution {
+    public int LengthOfLongestSubstring(string s) {
+        var arr = new int[128];
+        var l = 0;
+        var r = 0;
+        var ans = 0;
+        while(r < s.Length)
+        {
+            if(arr[s[r]] == 0)
+            {
+                arr[s[r]]++;
+                ans = Math.Max(ans, r-l+1);
+            }else{
+                while(s[l] != s[r])
+                {
+                    arr[s[l++]]--;
+                }
+                l++;
+            }
+            
+            r++;
+        }
+        
+        return ans;
+    }
+}
