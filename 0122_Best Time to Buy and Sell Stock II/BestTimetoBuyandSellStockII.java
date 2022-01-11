c051ebbbd7ad0d0a610cfdbc3d3e77a2f1a1e20d
Jiajionline <Jiajionline@gmail.com>
2022-01-11 08:09:31 -0800

    Leetcode 122 Best Time to Buy and Sell Stock II

---
+++ 0122_Best Time to Buy and Sell Stock II/BestTimetoBuyandSellStockII.java
@@ -0,0 +1,9 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int ans = 0;
+        for(int i=1;i<prices.length;i++)
+            ans += Math.max(0, prices[i] - prices[i-1]);
+        
+        return ans;
+    }
+}
