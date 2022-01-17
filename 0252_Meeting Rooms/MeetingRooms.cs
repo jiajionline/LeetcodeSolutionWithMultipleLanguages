4b5163e550fb2014dab59820aa59acadc11408f3
Jiajionline <Jiajionline@gmail.com>
2022-01-17 15:13:09 -0800

    Leetcode 252 Meeting Rooms

--- 0252_Meeting Rooms/MeetingRooms.cs
+++ 0252_Meeting Rooms/MeetingRooms.cs
@@ -1,19 +1,8 @@
 public class Solution {
     public bool CanAttendMeetings(int[][] intervals) {
-        if(intervals == null || intervals.Length <= 1) return true;
-        
-        Array.Sort(intervals, (a,b) => {
-            return a[0].CompareTo(b[0]);
-        });
-        
-        var curMeeting = intervals[0];
-        
+        Array.Sort(intervals, (a,b) => { return a[0].CompareTo(b[0]); });
         for(int i=1;i<intervals.Length;i++)
-        {
-            if(curMeeting[1] > intervals[i][0]) return false;
-            curMeeting = intervals[i];
-        }
-            
+            if(intervals[i][0] < intervals[i-1][1]) return false;
         return true;
     }
 }
