4e6fdc30ef682e51897292639ca05ab2e66cc2d0
Jiajionline <Jiajionline@gmail.com>
2022-01-20 14:44:21 -0800

    Leetcode 221 Maximal Square

--- 0221_Maximal Square/MaximalSquare.cs
+++ 0221_Maximal Square/MaximalSquare.cs
@@ -1,24 +1,21 @@
 public class Solution {
     public int MaximalSquare (char[][] matrix) {
-        if (matrix == null || matrix.Length == 0) return 0;
-        int rowLength = matrix.Length;
-        int colLength = matrix[0].Length;
-        int[, ] dp = new int[rowLength, colLength];
-        int largestSize = 0;
-        for (int i = 0; i < rowLength; i++) {
-            for (int j = 0; j < colLength; j++) {
-                if (i == 0 || j == 0) {
+        var m = matrix.Length;
+        if(m == 0) return 0;
+        var n = matrix[0].Length;
+        int[,] dp = new int[m, n];
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if (i == 0 || j == 0) 
                     dp[i, j] = matrix[i][j] - '0';
-                } else if (matrix[i][j] - '0' == 0) {
-                    dp[i, j] = 0;
-                } else {
-                    dp[i, j] = Math.Min (Math.Min (dp[i - 1, j], dp[i - 1, j - 1]), dp[i, j - 1]) + (matrix[i][j] - '0');
-                }
+                else if(matrix[i][j] == '1')
+                    dp[i, j] = Math.Min(Math.Min(dp[i - 1, j], dp[i - 1, j - 1]), dp[i, j - 1]) + 1;
 
-                largestSize = Math.Max (dp[i, j], largestSize);
+                ans = Math.Max(dp[i, j], ans);
             }
         }
 
-        return largestSize * largestSize;
+        return ans * ans;
     }
 }
--- 0221_Maximal Square/MaximalSquare.java
+++ 0221_Maximal Square/MaximalSquare.java
@@ -1,16 +1,21 @@
 public class Solution {
     public int maximalSquare(char[][] matrix) {
-        int rows = matrix.length, cols = rows > 0 ? matrix[0].length : 0;
-        int[][] dp = new int[rows + 1][cols + 1];
-        int maxsqlen = 0;
-        for (int i = 1; i <= rows; i++) {
-            for (int j = 1; j <= cols; j++) {
-                if (matrix[i - 1][j - 1] == '1') {
+        int m = matrix.length;
+        if(m == 0) return 0;
+        int n = matrix[0].length;
+        int[][] dp = new int[m][n];
+        int ans = 0;
+        for (int i = 0; i < m; i++) {
+            for (int j = 0; j < n; j++) {
+                if(i == 0 || j == 0) {
+                    dp[i][j] = matrix[i][j] - '0';   
+                }else if (matrix[i][j] == '1') {
                     dp[i][j] = Math.min(Math.min(dp[i][j - 1], dp[i - 1][j]), dp[i - 1][j - 1]) + 1;
-                    maxsqlen = Math.max(maxsqlen, dp[i][j]);
                 }
+                
+                ans = Math.max(ans, dp[i][j]);
             }
         }
-        return maxsqlen * maxsqlen;
+        return ans * ans;
     }
 }
