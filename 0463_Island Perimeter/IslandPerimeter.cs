e4ff7cb0ed066dc95bd9db5bb9947ee7544f3393
Jiajionline <Jiajionline@gmail.com>
2022-01-20 21:31:16 -0800

    Leetcode 463 Island Perimeter

--- 0463_Island Perimeter/IslandPerimeter.cs
+++ 0463_Island Perimeter/IslandPerimeter.cs
@@ -1,39 +1,27 @@
-﻿public class Solution
-{
-    public int IslandPerimeter(int[][] grid)
-    {
-        int perimeter = 0;
-        for (int i = 0; i < grid.Length; i++)
-        {
-            for (int j = 0; j < grid[i].Length; j++)
+﻿public class Solution {
+    public int IslandPerimeter(int[][] grid) {
+        var ans = 0;
+        
+        var m = grid.Length;
+        var n = grid[0].Length;
+        
+        var dirs = new int[]{1,0,-1,0,1};
+        
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
             {
-                if (grid[i][j] == 1)
+                if(grid[i][j] == 0) continue;
+                
+                ans += 4;
+                for(int k=0;k<4;k++)
                 {
-                    perimeter += 4;
-                    if (i - 1 >= 0 && grid[i - 1][j] == 1)
-                    {
-                        perimeter--;
-                    }
-
-                    if (j - 1 >= 0 && grid[i][j - 1] == 1)
-                    {
-                        perimeter--;
-                    }
-
-                    if (i + 1 < grid.Length && grid[i + 1][j] == 1)
-                    {
-                        perimeter--;
-                    }
-
-                    if (j + 1 < grid[i].Length && grid[i][j + 1] == 1)
-                    {
-                        perimeter--;
-                    }
+                    var dx = dirs[k] + i;
+                    var dy = dirs[k+1] + j;
+                    if(dx >= 0 && dx < m && dy >=0 && dy < n && grid[dx][dy] == 1)
+                        ans -=1;
                 }
             }
-        }
-
-        return perimeter;
+        
+        return ans;
     }
 }
-
