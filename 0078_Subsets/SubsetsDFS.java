232679c5e404a0d1ac6c2c206d668dca3d2f8547
Jiajionline <Jiajionline@gmail.com>
2022-01-20 18:49:57 -0800

    Leetcode 78 Subsets

--- 0078_Subsets/SubsetsDFS.java
+++ 0078_Subsets/SubsetsDFS.java
@@ -1,24 +1,17 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<List<Integer>>();
-
-        for(int n=0;n<=nums.length;n++){
-            DFS(nums, 0,n,new ArrayList<Integer>(),ans);
-        }
-
+        DFS(nums, 0, new ArrayList<Integer>(), ans);
         return ans;
     }
-
-    private void DFS(int[] nums, int s, int n, List<Integer> cur, List<List<Integer>> ans){
-        if(cur.size() == n){
-            ans.add(new ArrayList(cur));
-            return;
-        }
-
-        for(int i=s;i<nums.length;i++){
-            cur.add(nums[i]);
-            DFS(nums, i+1,n,cur,ans);
-            cur.remove(cur.size()-1);
+    
+    private void DFS(int[] nums, int index, List<Integer> list, List<List<Integer>> ans){
+            ans.add(new ArrayList(list));
+        
+        for(int i=index;i< nums.length;i++){
+            list.add(nums[i]);
+            DFS(nums, i+1, list,ans);
+            list.remove(list.size()-1);
         }
     }
 }
