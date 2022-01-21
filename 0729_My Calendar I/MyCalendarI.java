5cca39d1bfd372c6f00a21d34a8d71c870aa1d65
Jiajionline <Jiajionline@gmail.com>
2022-01-20 18:50:32 -0800

    Leetcode 729 My Calendar I

--- 0729_My Calendar I/MyCalendarI.java
+++ 0729_My Calendar I/MyCalendarI.java
@@ -1,5 +1,3 @@
-import java.util.TreeMap;
-
 class MyCalendar {
     private TreeMap<Integer, Integer> _booked;
     public MyCalendar() {
@@ -7,8 +5,11 @@ class MyCalendar {
     }
 
     public boolean book(int start, int end) {
+        //The floorKey() method is used to return the greatest key less than or equal to given key from the parameter.
         Integer lb = _booked.floorKey(start);
+        //get end mapped by lb
         if(lb != null && _booked.get(lb) > start) return false;
+        //ceilingKey() returns the least key greater than or equal to the given key or null if the such a key is absent.
         Integer ub = _booked.ceilingKey(start);
         if(ub != null && ub < end) return false;
         _booked.put(start, end);
