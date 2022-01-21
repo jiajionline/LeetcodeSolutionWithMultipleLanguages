ceae78c604d10683883a4620e3f3d56a5f8ed73d
Jiajionline <Jiajionline@gmail.com>
2022-01-20 19:21:43 -0800

    Leetcode 40 Combination Sum II

--- 0040_Combination Sum II/CombinationSumII.cs
+++ 0040_Combination Sum II/CombinationSumII.cs
@@ -1,25 +1,26 @@
 public class Solution {
     public IList<IList<int>> CombinationSum2(int[] candidates, int target) {
-        var ans = new List<IList<int>>();
-        if(candidates == null || candidates.Length == 0) return ans;
         Array.Sort(candidates);
+        var ans = new List<IList<int>>();
         DFS(candidates, target, 0, new List<int>(), ans);
         return ans;
     }
     
-    private void DFS(int[] candidates, int target, int s, IList<int> curr, IList<IList<int>> ans){
-        if(target == 0){
-            ans.Add(new List<int>(curr));
+    private void DFS(int[] candidates, int target, int index, List<int> list, List<IList<int>> ans)
+    {
+        if(target == 0) 
+        {
+            ans.Add(new List<int>(list));
             return;
         }
         
-        for(int i=s;i<candidates.Length;i++){
+        for(int i=index;i<candidates.Length;i++)
+        {
+            if(i > index && candidates[i] == candidates[i-1]) continue;
             if(candidates[i] > target) break;
-            if(i>s && candidates[i] == candidates[i-1]) continue;
-            
-            curr.Add(candidates[i]);
-            DFS(candidates, target - candidates[i], i+1, curr, ans);
-            curr.RemoveAt(curr.Count - 1);
+            list.Add(candidates[i]);
+            DFS(candidates, target - candidates[i], i+1, list, ans);
+            list.RemoveAt(list.Count - 1);
         }
     }
 }
