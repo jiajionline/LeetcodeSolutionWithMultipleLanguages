7ded6b63514f8f1c7a0749299a971777c6767923
Jiajionline <Jiajionline@gmail.com>
2022-01-24 06:10:34 -0800

    Leetcode 454 4Sum II

--- 0454_4Sum II/4SumII.cs
+++ 0454_4Sum II/4SumII.cs
@@ -1,26 +1,23 @@
 public class Solution {
-    public int FourSumCount(int[] A, int[] B, int[] C, int[] D) {
-        if(A == null || B == null || C == null || D == null) return 0;
+    public int FourSumCount(int[] nums1, int[] nums2, int[] nums3, int[] nums4) {
+        var ans = 0;
         var dict = new Dictionary<int,int>();
-        int ans = 0;
         
-        foreach(var a in A){
-            foreach(var b in B){
-                if(!dict.ContainsKey(a + b)){
-                    dict.Add(a+b,0);
-                }
-                
-                dict[a+b] +=1;
+        foreach(var v1 in nums1)
+            foreach(var v2 in nums2)
+            {
+                var sum = v1 + v2;
+                if(!dict.ContainsKey(sum)) dict.Add(sum, 0);
+                dict[sum]++;
             }
-        }
         
-        foreach(var c in C){
-            foreach(var d in D){
-                if(dict.ContainsKey(-(c+d))){
-                    ans += dict[-(c+d)];
-                }
+        foreach(var v3 in nums3)
+            foreach(var v4 in nums4)
+            {
+                var sum = 0 - (v3 + v4);
+                if(dict.ContainsKey(sum))
+                    ans += dict[sum];
             }
-        }
         
         return ans;
     }
