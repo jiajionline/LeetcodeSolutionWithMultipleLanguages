3c46c3530a84ee0e18404a477201718d689e83ef
Jiajionline <Jiajionline@gmail.com>
2022-01-24 15:21:33 -0800

    Leetcode 111 Minimum Depth of Binary Tree

---
+++ 0111_Minimum Depth of Binary Tree/MinimumDepthofBinaryTree_PreOrder.cs
@@ -0,0 +1,18 @@
+﻿public class Solution {
+    private int ans = int.MaxValue;
+    public int MinDepth(TreeNode root) {
+        if(root == null) return 0;
+        Depth(root,1);
+        return ans;
+    }
+    
+    private void Depth(TreeNode node, int depth)
+    {
+        if(node == null) return;
+        if(node.left == null && node.right == null)
+            ans = Math.Min(ans, depth);
+        
+        Depth(node.left, depth+1);
+        Depth(node.right, depth+1);
+    }
+}
