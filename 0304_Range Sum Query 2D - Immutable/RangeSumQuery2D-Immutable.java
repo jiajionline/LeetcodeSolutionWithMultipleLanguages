a0eb4b6e25bca22e3cbe779e7cf14b37a69b706c
Jiajionline <Jiajionline@gmail.com>
2022-01-25 09:31:40 -0800

    Leetcode 304 Range Sum Query 2D - Immutable

--- 0304_Range Sum Query 2D - Immutable/RangeSumQuery2D-Immutable.java
+++ 0304_Range Sum Query 2D - Immutable/RangeSumQuery2D-Immutable.java
@@ -1,33 +1,33 @@
 class NumMatrix {
-
-    private int[][] dp;
+    private int[][] presum;
     public NumMatrix(int[][] matrix) {
-        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return;
+        int m = matrix.length;
+        int n = matrix[0].length;
+        presum = new int[m][n];
+        presum[0][0] = matrix[0][0];
+        for(int i=1;i<m;i++)
+            presum[i][0] = presum[i-1][0] + matrix[i][0];
         
-        var m = matrix.length;
-        var n = matrix[0].length;
-        dp = new int[m+1][n+1];
+        for(int i=1;i<n;i++)
+            presum[0][i] = presum[0][i-1] + matrix[0][i];
         
-        for(int i=1;i<=m;i++){
-            for(int j=1;j<=n;j++){
-                dp[i][j] = dp[i-1][j] + dp[i][j-1] - dp[i-1][j-1] + matrix[i-1][j-1];
-            }
-        }
+        for(int i=1;i<m;i++)
+            for(int j=1;j<n;j++)
+                presum[i][j] = presum[i-1][j] + presum[i][j-1] - presum[i-1][j-1] + matrix[i][j];
+            
     }
     
     public int sumRegion(int row1, int col1, int row2, int col2) {
-        var minI = Math.min(row1, row2);
-        var maxI = Math.max(row1, row2);
-        var minJ = Math.min(col1, col2);
-        var maxJ = Math.max(col1, col2);
-        
-        return dp[maxI+1][maxJ+1] - dp[maxI+1][minJ] - dp[minI][maxJ+1] + dp[minI][minJ];
+        int d = presum[row2][col2];
+        int c = row1-1 >= 0 ? presum[row1-1][col2] : 0;
+        int b = col1 -1 >= 0 ? presum[row2][col1-1] : 0;
+        int a = (row1-1 >= 0 && col1-1 >= 0) ? presum[row1-1][col1-1] : 0; 
+        return d - c - b + a;
     }
 }
-
 
 /**
  * Your NumMatrix object will be instantiated and called as such:
  * NumMatrix obj = new NumMatrix(matrix);
- * int param_1 = obj.SumRegion(row1,col1,row2,col2);
+ * int param_1 = obj.sumRegion(row1,col1,row2,col2);
  */
