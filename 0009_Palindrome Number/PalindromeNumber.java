ce68131e015d6cb9c6244683322b6b992fb6897d
Jiajionline <Jiajionline@gmail.com>
2022-01-26 07:06:19 -0800

    Leetcode 9 Palindrome Number

--- 0009_Palindrome Number/PalindromeNumber.java
+++ 0009_Palindrome Number/PalindromeNumber.java
@@ -1,17 +1,16 @@
-
-public class Solution {
+class Solution {
     public boolean isPalindrome(int x) {
-        if(x == Integer.MIN_VALUE) return false;
-        long y = Math.abs(x);
-        long value = 0;
+        long n = x;
+        long m = n;
+        long k = 0;
         
-        while(y>0){
-        	value *= 10;
-        	value += (y % 10);
-        	y /= 10;
+        while(m > 0)
+        {
+            k *= 10;
+            k += (m % 10);
+            m /= 10;
         }
         
-        if(value > Integer.MAX_VALUE) return false;
-        return x == (int)value;
+        return k == n;
     }
 }
