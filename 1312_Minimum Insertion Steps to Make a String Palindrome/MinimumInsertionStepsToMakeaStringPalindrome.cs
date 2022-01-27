b570c4e53127964946ae1290ef823a16aa80e9b9
Jiajionline <Jiajionline@gmail.com>
2022-01-27 05:45:11 -0800

    Leetcode 1312 Minimum Insertion Steps to Make a String Palindrome

--- 1312_Minimum Insertion Steps to Make a String Palindrome/MinimumInsertionStepsToMakeaStringPalindrome.cs
+++ 1312_Minimum Insertion Steps to Make a String Palindrome/MinimumInsertionStepsToMakeaStringPalindrome.cs
@@ -1,17 +1,23 @@
 public class Solution {
     public int MinInsertions(string s) {
-        if(string.IsNullOrEmpty(s)) return 0;
         var n = s.Length;
-        var dp = new int[n, n];
+        var dp = new int[n,n];
         
-        for(var len = 2;len <=n;len++)
-            for(var l = 0; l <= n- len;l++)
+        for(var len = 2; len <= n; len++)
+            for(var l = 0; l <= n-len;l++) 
             {
                 var r = l + len - 1;
+                
                 if(s[l] == s[r])
-                    dp[l,r] = dp[l+1,r-1];
-                else
-                    dp[l,r] = Math.Min(dp[l+1,r], dp[l,r-1]) + 1;
+                {
+                    // don't have to add len > 3 check (just call dp[l,r] = dp[l+1,r-1]; ) , but it's more clear.
+                    if(len > 3)
+                        dp[l,r] = dp[l+1,r-1];
+                    else
+                        dp[l,r] = 0;
+                }else {
+                    dp[l,r] = Math.Min(dp[l+1,r], dp[l, r-1]) + 1;
+                }
             }
         
         return dp[0,n-1];
