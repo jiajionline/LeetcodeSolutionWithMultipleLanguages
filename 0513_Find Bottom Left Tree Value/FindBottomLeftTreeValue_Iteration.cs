0228ffd5b3547709f6d9f677dc55d9dbddac5f16
Jiajionline <Jiajionline@gmail.com>
2022-01-28 06:54:34 -0800

    Leetcode 513 Find Bottom Left Tree Value

---
+++ 0513_Find Bottom Left Tree Value/FindBottomLeftTreeValue_Iteration.cs
@@ -0,0 +1,19 @@
+﻿class Solution
+{
+    public int FindBottomLeftValue(TreeNode root)
+    {
+        var queue = new Queue<TreeNode>();
+        TreeNode mostLeft = null;
+        queue.Enqueue(root);
+
+        while(queue.Count > 0)
+        {
+            mostLeft = queue.Dequeue();
+            if (mostLeft.right != null) queue.Enqueue(mostLeft.right);
+            if (mostLeft.left != null) queue.Enqueue(mostLeft.left);
+        }
+
+        return mostLeft.val;
+    }
+}
+
---
+++ 0513_Find Bottom Left Tree Value/FindBottomLeftTreeValue_Recursion.cs
@@ -0,0 +1,21 @@
+﻿public class Solution {
+    private int _ans = 0;
+    private int _level = 0;
+    public int FindBottomLeftValue(TreeNode root) {
+        Traverse(root, 1);
+        return _ans;
+    }
+    
+    private void Traverse(TreeNode node, int level)
+    {
+        if(node == null) return;
+        if(level > _level)
+        {
+            _level = level;
+            _ans = node.val;
+        }
+        
+        Traverse(node.left, level+1);
+        Traverse(node.right, level + 1);
+    }
+}
