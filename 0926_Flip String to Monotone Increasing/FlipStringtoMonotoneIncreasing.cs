4daed9f5658aa46e43d18519f662261e3f89652e
Jiajionline <Jiajionline@gmail.com>
2022-02-10 06:55:11 -0800

    Leetcode 926 Flip String to Monotone Increasing

--- 0926_Flip String to Monotone Increasing/FlipStringtoMonotoneIncreasing.cs
+++ 0926_Flip String to Monotone Increasing/FlipStringtoMonotoneIncreasing.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public int MinFlipsMonoIncr(string S) {
         var sLen = S.Length;
-        var l = new int[sLen+1];
-        var r = new int[sLen+1];
+        var l = new int[sLen];
+        var r = new int[sLen];
         l[0] = S[0] - '0';
         r[sLen-1] = '1' - S[sLen-1];
 
@@ -14,9 +14,9 @@ public class Solution {
             r[i] = r[i+1] + ('1' - S[i]);
         }
 
-        //the case all 1's
-        int ans = r[0]; 
-        for(int i=1;i<=sLen;i++){
+        //the case all 1's (r[0]) or all 0's (l[sLen-1])
+        int ans = Math.Min(r[0], l[sLen-1]); 
+        for(int i=1;i<sLen;i++){
             ans = Math.Min(ans, l[i-1] + r[i]);
         }
 
