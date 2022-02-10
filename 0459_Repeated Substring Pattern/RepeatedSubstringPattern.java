78778d1563c7a393072e7e4b81471b2bf3757c3d
Jiajionline <Jiajionline@gmail.com>
2022-02-10 06:06:15 -0800

    Leetcode 459 Repeated Substring Pattern

---
+++ 0459_Repeated Substring Pattern/RepeatedSubstringPattern.java
@@ -0,0 +1,21 @@
+﻿class Solution {
+    public boolean repeatedSubstringPattern(String s) {
+        int n = s.length();
+        if(n <= 1) return false;
+        
+        for(int len=n/2;len >=1 ; len--) {
+            if(n % len == 0)
+            {
+                int numRepeats = n / len;
+                String substring = s.substring(0, len);
+                StringBuilder sb = new StringBuilder();
+                for(int i=1;i<=numRepeats;i++)
+                    sb.append(substring);
+                
+                if(sb.toString().equals(s)) return true;
+            }
+        }
+        
+        return false;
+    }
+}
