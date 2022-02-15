f943e6bcb3aba0cc102b387cd690c81dce84fbea
Jiajionline <Jiajionline@gmail.com>
2022-02-15 06:54:57 -0800

    Leetcode 654 Maximum BinaryTree

--- 0654_MaximumBinaryTree/MaximumBinaryTree.cs
+++ 0654_MaximumBinaryTree/MaximumBinaryTree.cs
@@ -1,40 +1,27 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int x) { val = x; }
- * }
- */
 public class Solution {
-    public TreeNode ConstructMaximumBinaryTree (int[] nums) {
-        if (nums == null || nums.Length == 0) return null;
-        return BuildTree (nums, 0, nums.Length - 1);
+    public TreeNode ConstructMaximumBinaryTree(int[] nums) {
+        return ConstructMaximumBinaryTree(nums, 0, nums.Length-1);
     }
-
-    private TreeNode BuildTree (int[] nums, int left, int right) {
-        if (left > right) return null;
-        if (right > nums.Length) return null;
-        if (left == right) {
-            return new TreeNode (nums[left]);
-        }
-        //find the index of maxinum value
-        var index = FindMax (nums, left, right);
-        TreeNode root = new TreeNode (nums[index]);
-        root.left = BuildTree (nums, left, index - 1);
-        root.right = BuildTree (nums, index + 1, right);
-
+    
+    private TreeNode ConstructMaximumBinaryTree(int[] nums, int l , int r)
+    {
+        if(l > r) return null;
+        
+        var index = FindMaxIndex(nums, l , r);
+        var root = new TreeNode(nums[index]);
+        root.left = ConstructMaximumBinaryTree(nums, l , index-1);
+        root.right = ConstructMaximumBinaryTree(nums, index+1, r);
         return root;
     }
-
-    private int FindMax (int[] nums, int left, int right) {
-        int maxIndex = left;
-        for (int i = left + 1; i <= right; i++) {
-            if (nums[i] > nums[maxIndex]) {
-                maxIndex = i;
-            }
+    
+    private int FindMaxIndex(int[] nums, int l , int r)
+    {
+        var index = l;
+        for(int i=l+1;i<=r;i++)
+        {
+            if(nums[i] > nums[index]) index = i;
         }
-        return maxIndex;
+        
+        return index;
     }
 }
