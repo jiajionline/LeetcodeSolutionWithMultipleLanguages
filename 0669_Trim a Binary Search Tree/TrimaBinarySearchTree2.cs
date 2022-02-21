06d5855094a24cef5db11af4b5b2bdbf14512df2
Jiajionline <Jiajionline@gmail.com>
2022-02-21 11:41:29 -0800

    Leetcode 669 Trim a Binary Search Tree

--- 0669_Trim a Binary Search Tree/TrimaBinarySearchTree2.cs
+++ 0669_Trim a Binary Search Tree/TrimaBinarySearchTree2.cs
@@ -1,29 +1,18 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     public int val;
- *     public TreeNode left;
- *     public TreeNode right;
- *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 public class Solution {
     public TreeNode TrimBST(TreeNode root, int low, int high) {
         if(root == null) return null;
         var left = TrimBST(root.left, low, high);
         var right = TrimBST(root.right, low, high);
-        root.left = left;
-        root.right = right;
         
         if(root.val < low)
             return right;
         else if(root.val > high) 
             return left;
-        else
-            return root;
+        else {
+             root.left = left;
+             root.right = right;
+             return root;
+        }
+            
     }
 }
