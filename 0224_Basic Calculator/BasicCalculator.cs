3241ad2800bf53d94978216302c60d45f6d27ff9
Jiajionline <Jiajionline@gmail.com>
2022-03-09 14:17:57 -0800

    Leetcode 224 Basic Calculator

--- 0224_Basic Calculator/BasicCalculator.cs
+++ 0224_Basic Calculator/BasicCalculator.cs
@@ -1,38 +1,38 @@
 public class Solution
+{
+    public int Calculate(string s)
     {
-        public int Calculate(string s)
+        if (string.IsNullOrWhiteSpace(s)) return 0;
+
+        int result = 0;
+        int sign = 1;
+        int num = 0;
+        var stack = new Stack<int>();
+        stack.Push(sign);
+
+        foreach (var c in s)
         {
-            if (string.IsNullOrWhiteSpace(s)) return 0;
-
-            int result = 0;
-            int sign = 1;
-            int num = 0;
-            var stack = new Stack<int>();
-            stack.Push(sign);
-
-            foreach (var c in s)
+            if (char.IsDigit(c))
             {
-                if (char.IsDigit(c))
-                {
-                    num = num * 10 + (c - '0');
-                }
-                else if (c == '+' || c == '-')
-                {
-                    result += sign * num;
-                    sign = stack.Peek() * (c == '+' ? 1 : -1);
-                    num = 0;
-                }
-                else if (c == '(')
-                {
-                    stack.Push(sign);
-                }
-                else if (c == ')')
-                {
-                    stack.Pop();
-                }
+                num = num * 10 + (c - '0');
             }
-
-            result += sign * num;
-            return result;
+            else if (c == '+' || c == '-')
+            {
+                result += sign * num;
+                sign = stack.Peek() * (c == '+' ? 1 : -1);
+                num = 0;
+            }
+            else if (c == '(')
+            {
+                stack.Push(sign);
+            }
+            else if (c == ')')
+            {
+                stack.Pop();
+            }
         }
+
+        result += sign * num;
+        return result;
     }
+}
