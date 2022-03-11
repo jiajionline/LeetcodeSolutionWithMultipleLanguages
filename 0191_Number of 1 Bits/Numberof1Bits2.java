677828efda83a8c8480445d7b1090aaf91401125
Jiajionline <Jiajionline@gmail.com>
2022-03-10 20:03:49 -0800

    Leetcode 191 Number of 1 Bits

--- 0191_Number of 1 Bits/Numberof1Bits2.java
+++ 0191_Number of 1 Bits/Numberof1Bits2.java
@@ -2,11 +2,15 @@ public class Solution {
     // you need to treat n as an unsigned value
 
     public int hammingWeight(int n) {
-        int sum = 0;
-        while (n != 0) {
-            sum++;
-            n &= (n - 1);
+        int ans = 0;
+        int mask = 1;
+        for (int i = 0; i < 32; i++) {
+            if ((n & mask) != 0) {
+                ans++;
+            }
+            mask <<= 1;
         }
-        return sum;
+    
+        return ans;
     }
 }
