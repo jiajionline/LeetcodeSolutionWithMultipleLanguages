dcd35d07ea8dc71fd42902ec8febd0b802241237
Jiajionline <Jiajionline@gmail.com>
2022-03-26 15:38:30 -0700

    Leetcode 68 Text Justification

--- 0068_Text Justification/TextJustification.java
+++ 0068_Text Justification/TextJustification.java
@@ -1,11 +1,15 @@
 class Solution {
     public List<String> fullJustify(String[] words, int maxWidth) {
         List<String> ans = new ArrayList();
+        // the count of words
         int n = words.length;
         int index = 0;
+        // scan each word
         while(index < n) {
+            // initialize totalChars by the first word
             int totalChars = words[index].length();
             int last = index + 1;
+            // find the last word which overflow the line
             while(last < n) {
                 if(totalChars + 1 + words[last].length() > maxWidth) break;
                 totalChars += (1 + words[last].length());
@@ -13,10 +17,11 @@ class Solution {
             }
             
             // last become the first word in the next line
+            // the gaps mean how many gaps between words , for example : "This    is    an" has 2.
             int gaps = last - index - 1;
             StringBuilder sb = new StringBuilder();
             
-            //first case
+            //first case, if there is no gap (only one long word in the line) or this is the last line
             if(gaps == 0 || last == n) {
                 for(int i=index; i < last;i++) {
                     sb.append(words[i]);
@@ -27,12 +32,17 @@ class Solution {
                 while(sb.length() < maxWidth)
                     sb.append(' ');
             }else {
+                // how many apcaces per gap
                 int spaces = (maxWidth - totalChars) / gaps;
+                // some gaps will have more space
                 int rest = (maxWidth - totalChars) % gaps;
+                // handle from index to the last second in this line (last word need to be handled differentially )
                 for(int i=index; i < last - 1; i++) {
                     sb.append(words[i]);
+                    // since totalChars counted by the word + ' ' so that we need to add a ' ' firstly. The spaces don't count this space.
                     sb.append(' ');
                     for(int j=0;j<spaces;j++) sb.append(' ');
+                    // if rest > 0, for this gap, need to add one more space
                     if(rest > 0) {
                         sb.append(' ');
                         rest--;
