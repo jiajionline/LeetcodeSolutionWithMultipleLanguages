08ab5caad248c35d5209d701513dc34262a5ce50
Jiajionline <Jiajionline@gmail.com>
2022-03-29 14:38:41 -0700

    Leetcode 1008 Construct Binary Search Tree from Preorder Traversal

--- 1008_Construct Binary Search Tree from Preorder Traversal/ConstructBinarySearchTreefromPreorderTraversal.java
+++ 1008_Construct Binary Search Tree from Preorder Traversal/ConstructBinarySearchTreefromPreorderTraversal.java
@@ -1,31 +1,19 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
     public TreeNode bstFromPreorder(int[] preorder) {
-        return bstFromPreorder(preorder, 0, preorder.length);
+        return bstFromPreorder(preorder, 0, preorder.length - 1);
     }
     
-    private TreeNode bstFromPreorder(int[] preorder, int s, int e)
-    {
-        if(s >= e) return null;
-        TreeNode node = new TreeNode(preorder[s]);
-        int m = s;
-        while(m < e && preorder[m] <= node.val) m++;
-        node.left = bstFromPreorder(preorder, s+1, m);
-        node.right = bstFromPreorder(preorder, m, e);
-        return node;
+    private TreeNode bstFromPreorder(int[] preorder, int start, int end) {
+        if(start > end) return null;
+        TreeNode root = new TreeNode(preorder[start]);
+        
+        int index = start + 1;
+        for(; index <=end && preorder[index] < preorder[start];index++);
+        index--;
+        
+        root.left = bstFromPreorder(preorder, start + 1, index);
+        root.right = bstFromPreorder(preorder, index+1, end);
+        return root;
     }
 }
+
