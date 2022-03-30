12946d665ee9484dbd3a4d65ea75160ee5f95d13
Jiajionline <Jiajionline@gmail.com>
2022-03-30 12:29:22 -0700

    Leetcode 378 Kth Smallest Element in a Sorted Matrix

--- 0378_Kth Smallest Element in a Sorted Matrix/KthSmallestElementinaSortedMatrix2.java
+++ 0378_Kth Smallest Element in a Sorted Matrix/KthSmallestElementinaSortedMatrix2.java
@@ -1,29 +1,23 @@
 class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-      PriorityQueue<Node> minHeap = new PriorityQueue<Node>((n1, n2) -> matrix[n1.row][n1.col] - matrix[n2.row][n2.col]);
-      for (int i = 0; i < matrix.length && i < k; i++)
-      minHeap.add(new Node(i, 0));
-      int numberCount = 0, ans = 0;
-      while (!minHeap.isEmpty()) {
-        Node node = minHeap.poll();
-        ans = matrix[node.row][node.col];
-        if (++numberCount == k)
-          break;
-        node.col++;
-        if (matrix[0].length > node.col)
-          minHeap.add(node);
-      }
-      return ans;
-  }
+        PriorityQueue<int[]> pq = new PriorityQueue<int[]>((a,b) -> matrix[a[0]][a[1]] - matrix[b[0]][b[1]]);
+        int m = matrix.length;
+        
+        for(int i=0;i<m && i<k;i++) {
+            pq.add(new int[]{i,0});
+        }
+        
+        int count = 0;
+        
+        while(pq.size() > 0) {
+            int[] cell = pq.poll();
+            if(++count == k) return matrix[cell[0]][cell[1]];
+            
+            if(cell[1] < matrix[0].length-1) {
+                pq.add(new int[]{cell[0], cell[1]+1});
+            }
+        }
+        
+        return -1;
+    }
 }
-
-class Node {
-  int row;
-  int col;
-
-  Node(int row, int col) {
-    this.row = row;
-    this.col = col;
-  }
-}
-    
