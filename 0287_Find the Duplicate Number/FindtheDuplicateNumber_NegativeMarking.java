61b74c76ec521016dc1d024fec5c8ff0f30909f7
Jiajionline <Jiajionline@gmail.com>
2022-03-31 06:44:48 -0700

    Leetcode 287 Find the Duplicate Number

---
+++ 0287_Find the Duplicate Number/FindtheDuplicateNumber_NegativeMarking.java
@@ -0,0 +1,11 @@
+﻿class Solution {
+    public int findDuplicate(int[] nums) {
+        for(int i=0;i<nums.length;i++) {
+            int index = Math.abs(nums[i])-1;
+            if(nums[index] < 0) return Math.abs(nums[i]);
+            nums[index] = -nums[index];
+        }
+        
+        return 0;
+    }
+}
