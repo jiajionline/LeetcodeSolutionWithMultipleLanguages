d37f94781f1c24bd8e6c0ebf82b8424254f878cb
Jiajionline <Jiajionline@gmail.com>
2022-04-07 12:05:42 -0700

    Leetcode 930 Binary Subarrays With Sum

--- 0930_Binary Subarrays With Sum/BinarySubarraysWithSum.java
+++ 0930_Binary Subarrays With Sum/BinarySubarraysWithSum.java
@@ -1,22 +1,15 @@
 class Solution {
     public int numSubarraysWithSum(int[] nums, int goal) {
-        
-        int ans = 0;
-        HashMap<Integer,Integer> map = new HashMap<Integer,Integer>();
+        Map<Integer,Integer> map = new HashMap<>();
         map.put(0,1);
         int presum = 0;
-        
-        for(int v : nums)
-        {
+        int ans = 0;
+        for(int v : nums) {
             presum += v;
-            if(map.containsKey(presum - goal))
-                ans += map.get(presum - goal);
-            
-            if(!map.containsKey(presum)) map.put(presum,0);
-            map.put(presum, map.get(presum) + 1);
+            ans += map.getOrDefault(presum - goal, 0);
+            map.put(presum, map.getOrDefault(presum, 0) + 1);
         }
         
         return ans;
-        
     }
 }
