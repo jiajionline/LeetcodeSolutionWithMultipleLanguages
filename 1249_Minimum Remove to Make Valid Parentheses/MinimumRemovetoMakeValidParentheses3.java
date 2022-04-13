0d54936477b8b177073e8c00f2945538b8ba3349
Jiajionline <Jiajionline@gmail.com>
2022-04-12 20:16:34 -0700

    Leetcode Minimum Remove to Make Valid Parentheses

---
+++ 1249_Minimum Remove to Make Valid Parentheses/MinimumRemovetoMakeValidParentheses3.java
@@ -0,0 +1,27 @@
+class Solution {
+    public String minRemoveToMakeValid(String s) {
+        Set<Integer> indexesToRemove = new HashSet<>();
+        Stack<Integer> stack = new Stack<>();
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } if (s.charAt(i) == ')') {
+                if (stack.isEmpty()) {
+                    indexesToRemove.add(i);
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        while (!stack.isEmpty()) 
+            indexesToRemove.add(stack.pop());
+        
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            if (!indexesToRemove.contains(i)) {
+                sb.append(s.charAt(i));
+            }
+        }
+        return sb.toString();
+    }
+}
