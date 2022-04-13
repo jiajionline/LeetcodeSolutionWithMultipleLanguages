ad5a556d1ae44c760f28204abacf27f535efd15b
Jiajionline <Jiajionline@gmail.com>
2022-04-12 20:14:03 -0700

    Leetcode 56 Merge Intervals

--- 0056_Merge Intervals/MergeIntervals.java
+++ 0056_Merge Intervals/MergeIntervals.java
@@ -3,20 +3,16 @@ class Solution {
         if(intervals == null || intervals.length == 0)
             return intervals;
         
-        // sort intervals by starting value
         Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
         
-		// if end of previous interval is more than the start of current interval then there is a overlap
-        LinkedList<int[]> mergedIntervals = new LinkedList<>();
+        LinkedList<int[]> list = new LinkedList<>();
         for(int[] curr : intervals) {
-            //  if list empty or no overlap simply add current interval
-            if(mergedIntervals.isEmpty() || mergedIntervals.getLast()[1] < curr[0])
-                mergedIntervals.add(curr);
-            // else if overlap exists then merge curren tinterval with the previous interval
+            if(list.isEmpty() || list.getLast()[1] < curr[0])
+                list.add(curr);
             else 
-                mergedIntervals.getLast()[1] = Math.max(mergedIntervals.getLast()[1], curr[1]);
+                list.getLast()[1] = Math.max(list.getLast()[1], curr[1]);
         }
         
-        return mergedIntervals.toArray(new int[0][]);
+        return list.toArray(new int[list.size()][]);
     }
 }
