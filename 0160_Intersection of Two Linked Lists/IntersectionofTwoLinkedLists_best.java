8c743360af0e5448e8e9464fb24f3b1d65f62bcb
Jiajionline <Jiajionline@gmail.com>
2022-04-15 09:44:51 -0700

    Leetcode 160 Intersection of Two Linked Lists

---
+++ 0160_Intersection of Two Linked Lists/IntersectionofTwoLinkedLists_best.java
@@ -0,0 +1,13 @@
+﻿public class Solution {
+    public ListNode getIntersectionNode(ListNode headA, ListNode headB) {
+        ListNode pA = headA;
+        ListNode pB = headB;
+        
+        while(pA != pB) {
+            pA = pA == null ? headB : pA.next;
+            pB = pB == null ? headA : pB.next;
+        }
+        
+        return pA;
+    }
+}
