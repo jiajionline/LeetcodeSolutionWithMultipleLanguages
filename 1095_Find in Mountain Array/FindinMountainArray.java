4f9377ab0f370955f2d02c302049c3ad0e49846c
Jiajionline <Jiajionline@gmail.com>
2022-04-17 16:18:14 -0700

    Leetcode 1095 Find in Mountain Array

---
+++ 1095_Find in Mountain Array/FindinMountainArray.java
@@ -0,0 +1,55 @@
+/**
+ * // This is MountainArray's API interface.
+ * // You should not implement it, or speculate about its implementation
+ * interface MountainArray {
+ *     public int get(int index) {}
+ *     public int length() {}
+ * }
+ */
+ 
+class Solution {
+    public int findInMountainArray(int target, MountainArray mountainArr) {
+        // find peak
+        int l = 0, r = mountainArr.length() - 1;
+        
+        while(l < r) {
+            int mid = (r-l)/2 + l;
+            if(mountainArr.get(mid) < mountainArr.get(mid+1)) {
+                l = mid + 1;
+            }else {
+                r = mid;
+            }
+        }
+        
+        int peak = l;
+        
+        l = 0;
+        r = peak;
+        
+        while(l < r) {
+            int mid = (r-l)/2 + l;
+            if(mountainArr.get(mid) >= target) {
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
+        }
+        
+        if(mountainArr.get(l) == target) return l;
+        
+        l = peak + 1;
+        r = mountainArr.length();
+        while(l < r) {
+            int mid = (r-l)/2 + l;
+            if(mountainArr.get(mid) <= target) {
+                r = mid;
+            }else {
+                l = mid + 1;
+            }
+        }
+        
+        if(l < mountainArr.length() && mountainArr.get(l) == target) return l;
+        
+        return -1;
+    }
+}
