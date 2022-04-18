dbfc6870a967df95969c063e9b0797f0c1ae6383
Jiajionline <Jiajionline@gmail.com>
2022-04-18 11:49:42 -0700

    Leetcode 543 Diameter of Binary Tree

--- 0543_Diameter of Binary Tree/DiameterofBinaryTree.java
+++ 0543_Diameter of Binary Tree/DiameterofBinaryTree.java
@@ -1,28 +1,15 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     private int ans = 0;
     public int diameterOfBinaryTree(TreeNode root) {
-        LP(root);
+        postOrder(root);
         return ans;
     }
-
-    private int LP(TreeNode node){
-        if(node == null) return -1;
-        int left = LP(node.left);
-        int right = LP(node.right);
-        //plus one means add the edge between it's direct child and itself
-        int currentMax = Math.max(left, right) + 1;
-        ans = Math.max(ans, (left + 1) + (right + 1));
-
-        return currentMax;
+    
+    private int postOrder(TreeNode node) {
+        if(node == null) return 0;
+        int left = postOrder(node.left);
+        int right = postOrder(node.right);
+        ans = Math.max(ans, left + right);
+        return Math.max(left, right) + 1;
     }
-
 }
