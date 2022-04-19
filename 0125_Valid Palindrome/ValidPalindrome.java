259751dc3b6c35193661fcbc555f2b2c802fd988
Jiajionline <Jiajionline@gmail.com>
2022-04-19 06:45:24 -0700

    Leetcode 125 Valid Palindrome

---
+++ 0125_Valid Palindrome/ValidPalindrome.java
@@ -0,0 +1,13 @@
+﻿class Solution {
+    public boolean isPalindrome(String s) {
+        int l = 0 , r = s.length()-1;
+        
+        while(l < r) {
+            while(l < r && !Character.isLetterOrDigit(s.charAt(l))) l++;
+            while(l < r && !Character.isLetterOrDigit(s.charAt(r))) r--;
+            if(Character.toLowerCase(s.charAt(l++)) != Character.toLowerCase(s.charAt(r--))) return false;
+        }
+        
+        return true;
+    }
+}
