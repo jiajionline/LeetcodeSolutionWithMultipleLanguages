554add387e1a5d597a998c39da4a93461331e25f
Jiajionline <Jiajionline@gmail.com>
2022-04-25 15:48:49 -0700

    Leetcode 433 Minimum Genetic Mutation

--- 0433_Minimum Genetic Mutation/MinimumGeneticMutation_BFS.java
+++ 0433_Minimum Genetic Mutation/MinimumGeneticMutation_BFS.java
@@ -1,38 +1,43 @@
 class Solution {
+    private static final char[] GeneChars = new char[]{'A', 'C', 'G', 'T'};
+
     public int minMutation(String start, String end, String[] bank) {
-        if(start.equals(end)) return 0;
-        char[] genes = new char[]{'A','C','G','T'};
-        Set<String> banks = new HashSet<String>();
-        for(String b : bank)  banks.add(b);
         
-        Set<String> visited = new HashSet();
-        Queue<String> queue = new LinkedList();
+        Set<String> bankSet = new HashSet<>();
+        for(String gene : bank) bankSet.add(gene);
+        bankSet.add(start);
+        if(!bankSet.contains(end)) return -1;
+        
+        Queue<String> queue = new LinkedList<>();
         queue.add(start);
+        
+        Set<String> visited = new HashSet<String>();
+        visited.add(start);
+        
         int ans = 0;
         
         while(queue.size() > 0) {
-            int count = queue.size();
+            int size = queue.size();
+            ans++;
             
-            while(count-- > 0) {
-                String g = queue.poll();
-                if(g.equals(end)) return ans;
+            while(size-- > 0) {
+                char[] genes = queue.poll().toCharArray();
                 
-                visited.add(g);
-                char[] char_gene = g.toCharArray();
-                
-                for(int i=0;i<char_gene.length;i++) {
-                    char old_char = char_gene[i];
-                    for(int j=0;j<genes.length;j++) {
-                        char_gene[i] = genes[j];
-                        String newGene = new String(char_gene);
-                        if(!visited.contains(newGene) && banks.contains(newGene))
-                            queue.add(newGene);
+                for(int i=0;i< genes.length;i++) {
+                    char original = genes[i];
+                    for(int j=0;j<GeneChars.length;j++) {
+                        if(GeneChars[j] == original) continue;
+                        genes[i] = GeneChars[j];
+                        String newGene = new String(genes);
+                        if(visited.contains(newGene) || !bankSet.contains(newGene)) continue;
+                        if(newGene.equals(end)) return ans;
+                        queue.add(newGene);
+                        visited.add(newGene);
                     }
-                    char_gene[i] = old_char;
+                    
+                    genes[i] = original;
                 }
             }
-            
-            ans++;
         }
         
         return -1;
