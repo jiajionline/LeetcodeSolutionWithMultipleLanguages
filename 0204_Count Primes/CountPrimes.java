e86be4c75f1fe8f4e6e4ee88ffacf871b957ac24
Jiajionline <Jiajionline@gmail.com>
2022-04-26 05:36:44 -0700

    Leetcode 204 Count Primes

--- 0204_Count Primes/CountPrimes.java
+++ 0204_Count Primes/CountPrimes.java
@@ -1,16 +1,15 @@
 class Solution {
     public int countPrimes(int n) {
-      int ans = 0;
-      boolean[] isPrime = new boolean[n + 1];
-      Arrays.fill(isPrime, true);
-      isPrime[0] = false;
-      if (n > 0) isPrime[1] = false;
-      for (int i = 2; i < n; ++i) {
+        if(n <= 1) return 0;
+        int ans = 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        for (int i = 2; i < n; ++i) {
         if (!isPrime[i]) continue;
         ++ans;
         for (int j = 2 * i; j < n; j += i)
           isPrime[j] = false;
-      }
-      return ans;
+        }
+        return ans;
     }
   }
