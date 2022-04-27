da321202fba8160931a0b8819bd6f3e1f2c9a53e
Jiajionline <Jiajionline@gmail.com>
2022-04-27 05:46:02 -0700

    Leetcode 261 Graph Valid Tree

--- 0261_Graph Valid Tree/GraphValidTree_DFS.java
+++ 0261_Graph Valid Tree/GraphValidTree_DFS.java
@@ -1,23 +1,27 @@
 class Solution {
     public boolean validTree(int n, int[][] edges) {
-        List<List<Integer>> graph = new ArrayList(n);
-        for(int i=0;i<n;i++) graph.add(new ArrayList<Integer>());
-        for(int i=0;i<edges.length;i++) {
-            int v1 = edges[i][0];
-            int v2 = edges[i][1];
-            graph.get(v1).add(v2);
-            graph.get(v2).add(v1);
-        }
-        
-        Set<Integer> visited = new HashSet();
+        if(edges.length + 1 != n) return false;
+        Map<Integer, List<Integer>> graph = build(edges, n);
+        Set<Integer> visited = new HashSet<Integer>();
         visited.add(0);
-        var ans = DFS(graph, 0, -1, visited);
-        if(!ans) return false;
-        return visited.size() == n ? true : false;
-        
+        if(!DFS(graph, 0, -1, visited)) return false;
+        return visited.size() == n;
     }
     
-    private boolean DFS(List<List<Integer>> graph, int node, int parent, Set<Integer> visited) {
+    private Map<Integer, List<Integer>> build(int[][] edges, int n) {
+        Map<Integer, List<Integer>> graph = new HashMap<>();
+        for(int i=0;i<n;i++) 
+            graph.putIfAbsent(i, new ArrayList<Integer>());
+        for(int[] edge : edges) {
+            graph.get(edge[0]).add(edge[1]);
+            graph.get(edge[1]).add(edge[0]);
+        }
+        
+        return graph;
+    }
+    
+    private boolean DFS(Map<Integer, List<Integer>> graph, int node, int parent, Set<Integer> visited) {
+        
         for(int next : graph.get(node)) {
             if(next == parent) continue;
             if(visited.contains(next)) return false;
