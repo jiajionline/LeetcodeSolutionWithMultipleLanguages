b844d28dac29a0e3dbc507e1a1970c61b4f78c3e
Jiajionline <Jiajionline@gmail.com>
2022-05-06 06:28:44 -0700

    Leetcode 1047 Remove All Adjacent Duplicates In String

---
+++ 1047_Remove All Adjacent Duplicates In String/RemoveAllAdjacentDuplicatesInString2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String removeDuplicates(String s) {
+       StringBuilder sb = new StringBuilder();
+        for(char c : s.toCharArray()) {
+            if(sb.length() > 0 && sb.charAt(sb.length()-1) == c)
+                sb.deleteCharAt(sb.length() - 1);
+            else
+                sb.append(c);
+        }
+        
+        return sb.toString();
+    }
+}
