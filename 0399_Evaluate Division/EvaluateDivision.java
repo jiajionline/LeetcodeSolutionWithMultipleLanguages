4b2ce6f80e9fb8f3adf972392f5fecd63a32bec2
Jiajionline <Jiajionline@gmail.com>
2022-05-09 13:53:16 -0700

    Leetcode 399 Evaluate Division

--- 0399_Evaluate Division/EvaluateDivision.java
+++ 0399_Evaluate Division/EvaluateDivision.java
@@ -1,45 +1,48 @@
-import java.util.*;
-
 class Solution {
-    Map<String, HashMap<String, Double>> graph = new HashMap<>();
-
-    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {        
+    
+    public double[] calcEquation(List<List<String>> equations, double[] values, List<List<String>> queries) {
+        
+        Map<String, Map<String, Double>> graph = new HashMap<>();
+        
         for (int i = 0; i < equations.size(); ++i) {
             String start = equations.get(i).get(0);
             String end = equations.get(i).get(1);
             double val = values[i];
-            graph.computeIfAbsent(start, l -> new HashMap<String, Double>()).put(end, val);
-            graph.computeIfAbsent(end, l -> new HashMap<String, Double>()).put(start, 1.0 / val);
+            graph.putIfAbsent(start, new HashMap<String, Double>());
+            graph.get(start).put(end, val);
+            graph.putIfAbsent(end, new HashMap<String, Double>());
+            graph.get(end).put(start, 1.0/ val);
         }
-
+        
         double[] ans = new double[queries.size()];
-
+        
         for (int i = 0; i < queries.size(); ++i) {
             String start = queries.get(i).get(0);
             String end = queries.get(i).get(1);
             if (!graph.containsKey(start) || !graph.containsKey(end)) {
                 ans[i] = -1.0;
             } else {
-                ans[i] = DFS(start, end, new HashSet<String>());
+                ans[i] = DFS(start, end, new HashSet<String>(), graph);
             }
         }
-
+        
         return ans;
     }
-
-    private double DFS(String start, String end, Set<String> visited) {
-        if (start.equals(end))
-            return 1.0;
+    
+    private double DFS(String start, String end, Set<String> visited, Map<String, Map<String, Double>> graph) {
+        if (start.equals(end)) return 1.0;
         visited.add(start);
-        if (!graph.containsKey(start))
-            return -1.0;
-        for (String n : graph.get(start).keySet()) {
-            if (visited.contains(n))
-                continue;
-            double val = DFS(n, end, visited);
-            if (val > 0)
-                return val * graph.get(start).get(n);
+        if(!graph.containsKey(start)) return -1.0;
+        
+        for(String next : graph.get(start).keySet()) {
+            if(visited.contains(next)) continue;
+            double val = DFS(next, end, visited, graph);
+            if(val > 0) {
+                double curr = graph.get(start).get(next);
+                return val * curr;
+            }
         }
+        
         return -1.0;
     }
 }
