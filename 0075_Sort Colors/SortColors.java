78e386651894d89256205cf424bc9c9389fc4088
Jiajionline <Jiajionline@gmail.com>
2022-05-12 15:48:47 -0700

    Leetcode 75 Sort Colors

--- 0075_Sort Colors/SortColors.java
+++ 0075_Sort Colors/SortColors.java
@@ -1,23 +1,20 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int l = 0;
-        int r = nums.length-1;
-        int i = 0;
-        
+        int l = 0,  r = nums.length - 1, i = 0;
         while(i <= r) {
-            if(nums[i] == 0) {
-                int tmp = nums[l];
-                nums[l] = nums[i];
-                nums[i] = tmp;
-                l++;
+            if(nums[i] == 2) {
+                swap(nums, i, r--);
+            }else if(nums[i] == 0) {
+                swap(nums, i++, l++);
+            }else{
                 i++;
-            }else if(nums[i] == 2) {
-                int tmp = nums[r];
-                nums[r] = nums[i];
-                nums[i] = tmp;
-                r--;
-            }else
-                i++;
+            }
         }
+    }
+    
+    private void swap(int[] nums, int i, int j) {
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
     }
 }
