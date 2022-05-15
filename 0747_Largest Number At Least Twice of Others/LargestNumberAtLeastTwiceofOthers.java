c7363b35168e85c87cbc88c107b7666de1ef3fc0
Jiajionline <Jiajionline@gmail.com>
2022-05-15 13:48:48 -0700

    Leetcode 747 Largest Number At Least Twice of Others

--- 0747_Largest Number At Least Twice of Others/LargestNumberAtLeastTwiceofOthers.java
+++ 0747_Largest Number At Least Twice of Others/LargestNumberAtLeastTwiceofOthers.java
@@ -1,14 +1,22 @@
 class Solution {
     public int dominantIndex(int[] nums) {
-        int max = -1, index = -1, second = -1;
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > max) {
-                second = max;
-                max = nums[i];
-                index = i;
-            } else if (nums[i] > second)
-                second = nums[i];
+        if(nums.length <=1 ) return 0;
+        int secMax = 0;
+        int max = 1;
+        if(nums[1] < nums[0]) {
+            max = 0;
+            secMax = 1;
         }
-        return second * 2 <= max ? index : -1;
+        
+        for(int i=2;i<nums.length;i++) {
+            if(nums[i] > nums[max]) {
+                secMax = max;
+                max = i;
+            }else if(nums[i] > nums[secMax]) {
+                secMax = i;
+            }
+        }
+        
+        return nums[max] >= nums[secMax] * 2 ? max : -1;
     }
 }
