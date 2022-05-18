9c5d6afc99d6e3b413c752c28ba2c728b2d39194
Jiajionline <Jiajionline@gmail.com>
2022-05-17 20:13:20 -0700

    Leetcode 1051 Height Checker

---
+++ 1051_Height Checker/HeightChecker.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int heightChecker(int[] heights) {
+        int[] expected = heights.clone();
+        Arrays.sort(expected);
+        int ans = 0;
+        for(int i=0;i<heights.length;i++) {
+            if(heights[i] != expected[i]) ans++;
+        }
+        
+        return ans;
+    }
+}
