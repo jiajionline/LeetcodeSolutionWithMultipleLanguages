9f611b59f3e2c295d9686a423d2416747a3c462b
Jiajionline <Jiajionline@gmail.com>
2022-05-22 13:51:39 -0700

    Leetcode 79 Word Search

--- 0079_Word Search/WordSearch.java
+++ 0079_Word Search/WordSearch.java
@@ -1,29 +1,26 @@
 class Solution {
-    private int[] dirs = new int[]{0,1,0,-1,0};
     public boolean exist(char[][] board, String word) {
-        int m = board.length;
-        if(m == 0) return false;
-        int n = board[0].length;
-        for(int i=0;i<board.length;i++) {
-            for(int j=0;j<board[i].length;j++) {
-                if(DFS(board, word, 0, i, j)) return true;
-            }
+        int m = board.length, n = board[0].length;
+        int[] dirs = {0,1,0,-1,0};
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++)
+                if(DFS(board, i, j, m, n, 0, word, dirs)) return true;
         }
         
         return false;
     }
     
-    private boolean DFS(char[][] board, String word, int index, int x, int y) {
+    private boolean DFS(char[][] board, int x, int y, int m, int n, int index, String word, int[] dirs) {
         if(index == word.length()) return true;
-        if(x < 0 || y < 0 || x >= board.length || y >= board[x].length || board[x][y] != word.charAt(index)) return false;
+        if(x < 0 || y < 0 || x == m || y == n || board[x][y] != word.charAt(index)) return false;
         char c = board[x][y];
         board[x][y] = '#';
         for(int i=0;i<4;i++) {
-            int dx = x + dirs[i];
-            int dy = y + dirs[i+1];
-            if(DFS(board, word, index+1, dx, dy)) return true;
+            int dx = x + dirs[i], dy = y + dirs[i+1];
+            if(DFS(board, dx, dy, m, n, index+1, word, dirs)) return true;
         }
         board[x][y] = c;
+        
         return false;
     }
 }
