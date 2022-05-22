cd4fd8c288205f6356fec7a111269020a2f7d889
Jiajionline <Jiajionline@gmail.com>
2022-05-22 06:47:27 -0700

    Leetcode 211 Design Add and Search Words Data Structure

--- 0211_Design Add and Search Words Data Structure/DesignAddandSearchWordsDataStructure.java
+++ 0211_Design Add and Search Words Data Structure/DesignAddandSearchWordsDataStructure.java
@@ -1,39 +1,39 @@
-public class WordDictionary {
-    public class TrieNode {
-        public TrieNode[] children = new TrieNode[26];
-        public String item = "";
+class WordDictionary {
+    private TrieNode root;
+    public WordDictionary() {
+        root = new TrieNode();
     }
     
-    private TrieNode root = new TrieNode();
-
     public void addWord(String word) {
         TrieNode node = root;
-        for (char c : word.toCharArray()) {
-            if (node.children[c - 'a'] == null) {
-                node.children[c - 'a'] = new TrieNode();
-            }
+        for(char c : word.toCharArray()) {
+            if(node.children[c - 'a'] == null) node.children[c - 'a'] = new TrieNode();
             node = node.children[c - 'a'];
         }
-        node.item = word;
-    }
-
-    public boolean search(String word) {
-        return match(word.toCharArray(), 0, root);
+        node.isWord = true;
     }
     
-    private boolean match(char[] chs, int k, TrieNode node) {
-        if (k == chs.length) return !node.item.equals("");   
-        if (chs[k] != '.') {
-            return node.children[chs[k] - 'a'] != null && match(chs, k + 1, node.children[chs[k] - 'a']);
-        } else {
-            for (int i = 0; i < node.children.length; i++) {
-                if (node.children[i] != null) {
-                    if (match(chs, k + 1, node.children[i])) {
-                        return true;
-                    }
-                }
-            }
-        }
+    public boolean search(String word) {
+        return helper(word, 0, root);
+    }
+    
+    private boolean helper(String word, int index, TrieNode node) {
+        if(word.length() == index) return node.isWord;
+        char c = word.charAt(index);
+        if(c != '.')
+            return node.children[c - 'a'] != null && helper(word, index+1, node.children[c-'a']);
+        
+        for(int i=0;i<node.children.length;i++)
+            if(node.children[i] != null && helper(word, index + 1, node.children[i])) return true;
+        
         return false;
+    }
+    
+    class TrieNode {
+        TrieNode[] children;
+        boolean isWord;
+        public TrieNode() {
+            children = new TrieNode[26];
+        }
     }
 }
