19e3d0080fa1855b77858dd86c05276b1994c400
Jiajionline <Jiajionline@gmail.com>
2022-05-22 13:52:01 -0700

    Leetcode 212 Word Search II

--- 0212_Word Search II/WordSearchII.java
+++ 0212_Word Search II/WordSearchII.java
@@ -1,22 +1,23 @@
 class Solution {
     public List<String> findWords(char[][] board, String[] words) {
+        int m = board.length, n = board[0].length;
         List<String> ans = new LinkedList<>();
-        if(board == null || words == null) return ans;
-        int[] dirs = new int[]{0,1,0,-1,0};
-        Trie root = buildTrie(words);
+        if(m == 0) return ans;
+        int[] dirs = {0,1,0,-1,0};
+        TrieNode root = buildTrie(words);
         
-        for(int i=0;i<board.length;i++) {
-            for(int j=0;j<board[i].length;j++) {
+        for(int i=0;i<m;i++) {
+            for(int j=0;j<n;j++) {
                 Trie p = root;
-                DFS(board, p, i, j, dirs, ans);
+                DFS(board, p, i, j, m, n, dirs, ans);
             }
         }
         
         return ans;
     }
     
-    private void DFS(char[][] board, Trie p, int x, int y, int[] dirs, List<String> ans) {
-        if(x < 0 || y < 0 || x == board.length || y == board[x].length || board[x][y] == '#') return;
+    private void DFS(char[][] board, Trie p, int x, int y, int m, int n, int[] dirs, List<String> ans) {
+        if(x < 0 || y < 0 || x == m || y == n || board[x][y] == '#') return;
         char c = board[x][y];
         if(p.next[c-'a'] == null) return;
         if(p.next[c-'a'].word != null) {
@@ -28,17 +29,17 @@ class Solution {
         for(int i=0;i<4;i++) {
             int dx = x + dirs[i];
             int dy = y + dirs[i+1];
-            DFS(board, p.next[c-'a'], dx, dy, dirs, ans);
+            DFS(board, p.next[c-'a'], dx, dy, m, n, dirs, ans);
         }
         
         board[x][y] = c;
         
     }
     
-    private Trie buildTrie(String[] words) {
-        Trie root = new Trie();
+    private TrieNode buildTrie(String[] words) {
+        TrieNode root = new TrieNode();
         for(String word : words) {
-            Trie p = root;
+            TrieNode p = root;
             for(char c : word.toCharArray()) {
                 if(p.next[c-'a'] == null) p.next[c-'a'] = new Trie();
                 p = p.next[c-'a'];
@@ -51,7 +52,7 @@ class Solution {
     }
 }
 
-class Trie {
-    Trie[] next = new Trie[26];
+class TrieNode {
+    TrieNode[] next = new TrieNode[26];
     String word;
 }
