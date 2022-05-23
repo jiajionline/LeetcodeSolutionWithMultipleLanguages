54f9a8ead79bd183e424e5453e74aab140195b15
Jiajionline <Jiajionline@gmail.com>
2022-05-22 18:55:31 -0700

    Leetcode 232 Implement Queue using Stacks

--- 0232_Implement Queue using Stacks/ImplementQueueusingStacks.java
+++ 0232_Implement Queue using Stacks/ImplementQueueusingStacks.java
@@ -1,35 +1,32 @@
-public class MyQueue {
+class MyQueue {
+    private Stack<Integer> pushStack;
+    private Stack<Integer> popStack;
+
+    public MyQueue() {
+        pushStack = new Stack<Integer>();
+        popStack = new Stack<Integer>();
+    }
     
-	private Stack<Integer> enqueueStack = new Stack<Integer>();
-	private Stack<Integer> dequeueStack = new Stack<Integer>();
-	
-    /** Push element x to the back of queue. */
     public void push(int x) {
-        this.enqueueStack.push(x);
+        pushStack.push(x);
     }
     
-    /** Removes the element from in front of queue and returns that element. */
     public int pop() {
-        this.adjust();
-        return this.dequeueStack.pop();
+        peek();
+        return popStack.pop();
     }
     
-    /** Get the front element. */
     public int peek() {
-        this.adjust();
-        return this.dequeueStack.peek();
+        if(popStack.isEmpty()) {
+            while(pushStack.size() > 0) {
+                popStack.push(pushStack.pop());
+            }
+        }
+        
+        return popStack.peek();
     }
     
-    /** Returns whether the queue is empty. */
     public boolean empty() {
-        return this.enqueueStack.isEmpty() && this.dequeueStack.isEmpty();
-    }
-    
-    private void adjust(){
-    	if(this.dequeueStack.isEmpty()){
-    		while(this.enqueueStack.size() > 0){
-    			this.dequeueStack.push(this.enqueueStack.pop());
-    		}
-    	}
+        return pushStack.isEmpty() && popStack.isEmpty();
     }
 }
