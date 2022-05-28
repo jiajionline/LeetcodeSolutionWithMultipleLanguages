86569d9d0d9b4cc3f0aec54e306b554fe1e62fbb
Jiajionline <Jiajionline@gmail.com>
2022-05-28 06:38:19 -0700

    Leetcode 684 Redundant Connection

--- 0684_Redundant Connection/RedundantConnectionDFS.java
+++ 0684_Redundant Connection/RedundantConnectionDFS.java
@@ -1,40 +1,27 @@
 class Solution {
     public int[] findRedundantConnection(int[][] edges) {
-      List<List<Integer>> adjList = new ArrayList<>();
-      for(int i = 0; i < edges.length + 1; i++){
-          adjList.add(new ArrayList<>());
-      }
-      for(int[] edge : edges){
-        int v = edge[0];
-        int u = edge[1];
+        int n = edges.length;
+        List<List<Integer>> graph = new ArrayList<>();
+        for(int i=0;i<=n;i++) graph.add(new ArrayList<Integer>());
         
-        // notice, visited would reset everytime.
-        HashSet<Integer> visited = new HashSet<>();
-        
-        if(dfs(visited, u, v, adjList)){ // return true if there's a path between u and v already
-          // connect u and v directly make it a graph from tree
-          return edge;
+        for(int[] edge : edges) {
+            int u = edge[0], v = edge[1];
+            if(DFS(graph, u, v, new HashSet<Integer>())) return edge;
+            graph.get(u).add(v);
+            graph.get(v).add(u);
         }
-        // if the path doesnt exist 
         
-        adjList.get(u).add(v);
-        adjList.get(v).add(u);
-      }
-      return new int[]{};
+        return new int[2];
     }
-    private boolean dfs(HashSet<Integer> visited, int u, int v, List<List<Integer>> adjList){
-      // base case , when u and v are the same node 
-      if(u == v) return true;
-      
-      //mark u as visited
-      visited.add(u);
-      
-      
-      // check every neighbors
-      for(int next : adjList.get(u)){
-        if(visited.contains(next)) continue;
-        if(dfs(visited, next, v, adjList)) return true;
-      }
-      return false;
+    
+    private boolean DFS(List<List<Integer>> graph, int u, int v, Set<Integer> visited) {
+        if(u == v) return true;
+        visited.add(u);
+        for(Integer next : graph.get(u)) {
+            if(visited.contains(next)) continue;
+            if(DFS(graph, next, v, visited)) return true;
+        }
+        
+        return false;
     }
 }
