368f71d26bdbd439df5c658557d0bbc7b5ba856d
Jiajionline <Jiajionline@gmail.com>
2022-06-08 06:54:49 -0700

    Leetcode 253 Meeting Rooms II

--- 0253_Meeting Rooms II/MeetingRoomsII.java
+++ 0253_Meeting Rooms II/MeetingRoomsII.java
@@ -12,3 +12,22 @@ class Solution {
         return pq.size();
     }
 }
+
+// another similar solution
+
+class Solution {
+    public int minMeetingRooms(int[][] intervals) {
+        if(intervals == null) return 0;
+        Arrays.sort(intervals, (a,b) -> Integer.compare(a[0], b[0]));
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        
+        int ans = 0;
+        for(int[] interval : intervals) {
+            while(pq.size() > 0 && interval[0] >= pq.peek()) pq.poll();
+            pq.add(interval[1]);
+            ans = Math.max(ans, pq.size());
+        }
+        
+        return ans;
+    }
+}
