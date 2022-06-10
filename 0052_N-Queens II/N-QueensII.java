cb035398a72c59b8a392e3c67d070b804a730217
Jiajionline <Jiajionline@gmail.com>
2022-06-10 13:56:13 -0700

    Leetcode 52 Queens II

---
+++ 0052_N-Queens II/N-QueensII.java
@@ -0,0 +1,37 @@
+public class Solution {
+    private int ans = 0;
+    public int totalNQueens(int n) {
+        char[][] board = new char[n][n];
+        for(int i = 0; i < n; i++)
+            for(int j = 0; j < n; j++)
+                board[i][j] = '.';
+        placeQueen(board, 0);
+        return ans;
+    }
+    
+    private void placeQueen(char[][] board, int colIndex) {
+        if(colIndex == board.length) {
+            ans++;
+            return;
+        }
+        
+        for(int i = 0; i < board.length; i++) {
+            if(validate(board, i, colIndex)) {
+                board[i][colIndex] = 'Q';
+                placeQueen(board, colIndex + 1);
+                board[i][colIndex] = '.';
+            }
+        }
+    }
+    
+    private boolean validate(char[][] board, int x, int y) {
+        for(int i = 0; i < board.length; i++) {
+            for(int j = 0; j < y; j++) {
+                if(board[i][j] == 'Q' && (x + j == y + i || x + y == i + j || x == i))
+                    return false;
+            }
+        }
+        
+        return true;
+    }
+}
