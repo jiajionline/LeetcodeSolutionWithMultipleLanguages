c75ba9758208fe716506bc1595436934b7ea5bb3
Jiajionline <Jiajionline@gmail.com>
2022-06-24 11:48:37 -0700

    Leetcode 15 3Sum

--- 0015_3Sum/3Sum.java
+++ 0015_3Sum/3Sum.java
@@ -3,17 +3,16 @@ class Solution {
 		Arrays.sort(num);
 		List<List<Integer>> ans = new LinkedList<>(); 
 		for (int i = 0; i < num.length-2; i++) {
-			if (i == 0 || (i > 0 && num[i] != num[i-1])) {
-				int l = i+1, r = num.length-1, sum = 0 - num[i];
-				while (l < r) {
-					if (num[l] + num[r] == sum) {
-						ans.add(Arrays.asList(num[i], num[l], num[r]));
-						while (l < r && num[l] == num[l+1]) l++;
-						while (l < r && num[r] == num[r-1]) r--;
-						l++; r--;
-					} else if (num[l] + num[r] < sum) l++;
-					else r--;
-			   }
+			if (i > 0 && num[i] == num[i-1]) continue;
+			int l = i+1, r = num.length-1, sum = 0 - num[i];
+			while (l < r) {
+				if (num[l] + num[r] == sum) {
+					ans.add(Arrays.asList(num[i], num[l], num[r]));
+					while (l < r && num[l] == num[l+1]) l++;
+					while (l < r && num[r] == num[r-1]) r--;
+					l++; r--;
+				} else if (num[l] + num[r] < sum) l++;
+				else r--;
 			}
 		}
 		return ans;
