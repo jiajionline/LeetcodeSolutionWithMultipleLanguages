25513cad8acf381fcacf66c55d4e8d7f579830d5
Jiajionline <Jiajionline@gmail.com>
2022-07-19 08:45:26 -0700

    Leetcode 486 Predict the Winner

--- 0486_Predict the Winner/PredicttheWinner_Recursion_with_Memo.java
+++ 0486_Predict the Winner/PredicttheWinner_Recursion_with_Memo.java
@@ -1,18 +1,12 @@
 class Solution {
     public boolean PredictTheWinner(int[] nums) {
         int n = nums.length;
-        int[][] memo = new int[n][n];
-        for(int i=0;i<n;i++) {
-            for(int j=0;j<n;j++) {
-                memo[i][j] = Integer.MIN_VALUE;
-            }
-        }
-        return PredictTheWinner(nums, 0, nums.length-1, memo) >= 0;
+        return PredictTheWinner(nums, 0, nums.length-1, new Integer[n][n]) >= 0;
     }
     
-    private int PredictTheWinner(int[] nums, int l, int r, int[][] memo) {
+    private int PredictTheWinner(int[] nums, int l, int r, Integer[][] memo) {
         if(l == r) return nums[l];
-        if(memo[l][r] != Integer.MIN_VALUE) return memo[l][r];
+        if(memo[l][r] != null) return memo[l][r];
         int res = Math.max(nums[l] - PredictTheWinner(nums, l+1, r, memo), nums[r] - PredictTheWinner(nums, l , r-1, memo));
         memo[l][r] = res;
         return res;
