ad4d8d72e5f8002fa47f432707f7e42a30007428
Jiajionline <Jiajionline@gmail.com>
2022-09-22 06:48:08 -0700

    Leetcode 557 Reverse Words in a String III

---
+++ 0557_Reverse Words in a String III/ReverseWordsInAStringIII.java
@@ -0,0 +1,25 @@
+﻿class Solution {
+    public String reverseWords(String s) {
+        if(s == null || s.isEmpty()) return s;
+        int n = s.length();
+        char[] chars = s.toCharArray();
+        int l = 0, r = 0;
+        while(r < n) {
+            while(r < n && s.charAt(r) == ' ') r++;
+            l = r;
+            while(r < n && s.charAt(r) != ' ') r++;
+            reverse(chars, l, r-1);
+        }
+        
+        return new String(chars);
+    }
+    
+    private void reverse(char[] chars, int l, int r) {
+        if(l >= chars.length) return;
+        while(l < r) {
+            char tmp = chars[l];
+            chars[l++] = chars[r];
+            chars[r--] = tmp;
+        }
+    }
+}
