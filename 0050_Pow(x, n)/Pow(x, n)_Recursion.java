ee5cd4009197b703807e821c885eb2fef98cd980
Jiajionline <Jiajionline@gmail.com>
2022-09-29 05:35:14 -0700

    Leetcode 50 Pow(x, n)

--- 0050_Pow(x, n)/Pow(x, n)_Recursion.java
+++ 0050_Pow(x, n)/Pow(x, n)_Recursion.java
@@ -1,19 +1,16 @@
 class Solution {
     public double myPow(double x, int n) {
-        if(n < 0) {
-            x = 1/x;
-            n = -n;
-        }
         return pow(x, n);
     }
     
-    public double pow(double x, int n) {
-        if(n == 0) return 1;
-        double r = pow(x, n/2);
-        if(n % 2 == 0) {        
-            return r * r;
-        }else {
-            return r * r * x;
+     private double pow(double x, long n) {
+        if(n < 0) return 1 / pow(x, -n);
+        if(n == 0) return 1.0;
+        if(n % 2 == 0) {
+            double v = pow(x, n/2);
+            return v * v;
+        }else{
+            return x * pow(x, n-1);
         }
     }
 }
