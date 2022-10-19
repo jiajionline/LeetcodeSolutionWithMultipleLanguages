0099471113eb3a7fa94058cb7e567df257a637a6
Jiajionline <Jiajionline@gmail.com>
2022-10-19 16:19:59 -0700

    Leetcode 69 Sqrt(x)

--- 0069_Sqrt(x)/Sqrt(x).java
+++ 0069_Sqrt(x)/Sqrt(x).java
@@ -1,19 +1,16 @@
 class Solution {
     public int mySqrt(int x) {
-        if(x == 0) return 0;
-        long l = 1;
-        // need to convert x to long since if x is Integer.Max_Value, it overflows if it plus 1
-        long r = (long)x + 1;
-        //upperbound
+        if(x <= 1) return x;
+        int l = 1, r = x/2 + 1;
         while(l < r) {
-            long mid = (r-l)/2 + l;
-            if(mid > x / mid) {
-                r = mid;
-            }else {
-                l = mid + 1;
+            int m = (r-l)/2 + l;
+            if(m > x/m) {
+                r = m;
+            }else{
+                l = m+1;
             }
         }
         
-        return (int)l - 1;
+        return l-1;
     }
 }
