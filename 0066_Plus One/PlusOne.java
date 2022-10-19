f676da02665aca409539f582c61716ee506d6eb2
Jiajionline <Jiajionline@gmail.com>
2022-10-19 06:24:58 -0700

    Leetcode 66 Plus One

--- 0066_Plus One/PlusOne.java
+++ 0066_Plus One/PlusOne.java
@@ -1,31 +1,21 @@
-
-public class Solution {
+class Solution {
     public int[] plusOne(int[] digits) {
-        if(digits == null || digits.length == 0){
-        	int[] zeroArray = new int[0];
-        	return zeroArray;
+        int n = digits.length, carry = 1;
+        for(int i=n-1;i>=0;i--) {
+            int v = carry + digits[i];
+            carry = v / 10;
+            digits[i] = v % 10;
         }
         
-        int[] tempArray = new int[digits.length];
-        System.arraycopy(digits, 0, tempArray, 0, digits.length);
-        int carry = 1;
-        for(int i=digits.length - 1; i>=0 && carry > 0; i--){
-        	int v = digits[i];
-        	v += carry;
-        	tempArray[i] = v % 10;
-        	carry = v / 10;
+        if(carry > 0) {
+            int[] ans = new int[n+1];
+            ans[0] = carry;
+            for(int i=0;i<n;i++) {
+                ans[i+1] = digits[i];
+                return ans;
+            }
         }
         
-        if(carry == 0){
-        	return tempArray;
-        }else{
-        	int[] ret = new int[tempArray.length+1];
-        	ret[0] = carry;
-        	for(int i=0;i<tempArray.length;i++){
-        		ret[i+1] = tempArray[i];
-        	}
-        	
-        	return ret;
-        }
+        return digits;
     }
 }
