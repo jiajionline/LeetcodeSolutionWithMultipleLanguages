e663079325293b1245a8f969b12af3c1af88f5c3
Jiajionline <Jiajionline@gmail.com>
2022-11-10 05:47:05 -0800

    Leetcode 162 Find Peak Element

---
+++ 0162_Find Peak Element/FindPeakElement_BruteForce.java
@@ -0,0 +1,9 @@
+public class Solution {
+    public int findPeakElement(int[] nums) {
+        for (int i = 0; i < nums.length - 1; i++) {
+            if (nums[i] > nums[i + 1])
+                return i;
+        }
+        return nums.length - 1;
+    }
+}
