3540a6648fb8c408754a67e686c20a3f60665945
Jiajionline <Jiajionline@gmail.com>
2022-11-15 07:01:53 -0800

    Leetcode 202 Happy Number

--- 0202_Happy Number/HappyNumber.java
+++ 0202_Happy Number/HappyNumber.java
@@ -1,21 +1,20 @@
 class Solution {
     public boolean isHappy(int n) {
-        if(n <= 0) return false;
-        if(n == 1) return true;
-        Set<Integer> set = new HashSet();
-        
-        while(!set.contains(n)) {
-            set.add(n);
-            int num = 0;
-            while(n > 0) {
-                num += (n % 10) * (n % 10);
-                n /= 10;
-            }
-            
-            if(num == 1) return true;
-            n = num;
+        Set<Integer> set = new HashSet<>();
+        while(n != 1){
+            n = convert(n);
+            if(!set.add(n)) return false;
         }
         
-        return false;
+        return true;
+    }
+    
+    private int convert(int n){
+        int v = 0;
+        while(n > 0){
+            v += (n % 10) * (n % 10);
+            n /= 10;
+        }
+        return v;
     }
 }
