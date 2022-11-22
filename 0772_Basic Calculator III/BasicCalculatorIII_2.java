c60d8d937cb24b6c72a98452ac6d54b995424255
Jiajionline <Jiajionline@gmail.com>
2022-11-22 07:12:47 -0800

    Leetcode 772 Basic Calculator III

---
+++ 0772_Basic Calculator III/BasicCalculatorIII_2.java
@@ -0,0 +1,35 @@
+class Solution {
+    private int index = 0;
+    private final String Operators = "+-*/)";
+    public int calculate(String s) {
+        int num = 0;
+        char operator = '+';
+        ArrayDeque<Integer> stack = new ArrayDeque<>();
+        while(index < s.length()) {
+            char c = s.charAt(index++);
+            if(Character.isDigit(c)) num = num * 10 + (c - '0');
+            
+            if(c == '(') num = calculate(s);
+            
+            if(index >= s.length() || Operators.indexOf(c) >= 0){
+                if(operator == '+') 
+                    stack.push(num);
+                else if(operator == '-') 
+                    stack.push(-num);
+                else if(operator == '*') 
+                    stack.push(stack.pop() * num);
+                else
+                    stack.push(stack.pop() / num);
+                
+                operator = c;
+                num = 0;
+            }
+            
+            if(c == ')') break;
+        }
+        
+        int ans = 0;
+        for(int v : stack) ans+=v;
+        return ans;
+    }
+}
