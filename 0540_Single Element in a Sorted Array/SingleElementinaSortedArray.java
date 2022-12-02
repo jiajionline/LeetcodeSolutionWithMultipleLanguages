a6716eb037adc9d82d5bf9c6bc60d8d9534669f4
Jiajionline <Jiajionline@gmail.com>
2022-12-02 11:30:51 -0800

    Leetcode 540 Single Element in a Sorted Array

--- 0540_Single Element in a Sorted Array/SingleElementinaSortedArray.java
+++ 0540_Single Element in a Sorted Array/SingleElementinaSortedArray.java
@@ -1,12 +1,10 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        int n = nums.length;
-        int l = 0, r = n;
+        int l = 0, r = nums.length-1;
         
         while(l < r) {
             int m = (r-l)/2 + l;
-            if(m <= 0 || m+1 >= n) return nums[m];
-            var k = m % 2 == 0 ? m + 1 : m - 1;
+            int k = m % 2 == 0 ? m + 1 : m - 1;
             
             if(nums[m] == nums[k]) {
                 l = m + 1;
