22329a870d77203c07d19399c72d4d8cea79877e
Jiajionline <Jiajionline@gmail.com>
2022-12-19 05:26:45 -0800

    Leetcode 371 Sum of Two Integers

--- 0371_Sum of Two Integers/SumofTwoIntegers_Iteration.java
+++ 0371_Sum of Two Integers/SumofTwoIntegers_Iteration.java
@@ -1,7 +1,5 @@
 class Solution {
     public int getSum(int a, int b) {
-        if(a == 0) return b;
-        if(b == 0) return a;
         while(b!=0) {
             int carry = a & b;
             a = a ^ b;
