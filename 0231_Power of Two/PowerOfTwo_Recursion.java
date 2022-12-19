eef08eea8bdbf008db15db64499f5ebf45a01ec2
Jiajionline <Jiajionline@gmail.com>
2022-12-19 06:22:54 -0800

    Leetcode 231 Power of two

---
+++ 0231_Power of Two/PowerOfTwo_Recursion.java
@@ -0,0 +1,7 @@
+﻿class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n <= 0) return false;
+        if(n ==1) return true;
+        return n % 2 == 0 && isPowerOfTwo(n/2);
+    }
+}
