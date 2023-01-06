c2b6f995f3ef17b07081ca045c8d7d29083b35ee
Jiajionline <Jiajionline@gmail.com>
2023-01-06 06:09:04 -0800

    Leetcode 200 Number of Islands

--- 0200_Number of Islands/NumberofIslands_2.java
+++ 0200_Number of Islands/NumberofIslands_2.java
@@ -1,29 +1,25 @@
 class Solution {
     public int numIslands(char[][] grid) {
+        int[][] dirs = {{0,1},{1,0},{0,-1},{-1,0}};
         int ans = 0;
-        int[] dirs = new int[]{1,0,-1,0,1};
-
-        for(int i=0;i<grid.length;i++)
+        for(int i=0;i<grid.length;i++) {
             for(int j=0;j<grid[i].length;j++){
-                if(grid[i][j] == '1')
-                {
+                if(grid[i][j] == '1'){
                     ans++;
-                    DFS(grid, i, j, dirs);
+                    dfs(grid,i,j,dirs);
                 }
             }
+        }
         
         return ans;
     }
     
-    private void DFS(char[][] grid, int x, int y, int[] dirs)
-    {
+    private void dfs(char[][] grid, int x, int y,int[][] dirs) {
         if(x < 0 || y < 0 || x == grid.length || y == grid[x].length || grid[x][y] != '1') return;
-        grid[x][y] = '2';
-        
-        for(int i=0;i<4;i++){
-            int dx = x + dirs[i];
-            int dy = y + dirs[i+1];
-            DFS(grid, dx, dy, dirs);
+        grid[x][y] = '0';
+        for(int i=0;i<dirs.length;i++) {
+            int dx = dirs[i][0] + x, dy = dirs[i][1] + y;
+            dfs(grid, dx, dy, dirs);
         }
     }
 }
