3bb722e490b1600e27c652bc6b062ab751a42509
Jiajionline <Jiajionline@gmail.com>
2023-01-19 06:49:11 -0800

    Leetcode 739 Daily Temperatures

---
+++ 0739_Daily Temperatures/DailyTemperatures2.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] dailyTemperatures(int[] temperatures) {
+        int n = temperatures.length;
+        int[] ans = new int[n];
+        Stack<Integer> stack = new Stack<>();
+        for(int i=0;i<n;i++) {
+            while(!stack.isEmpty() && temperatures[i] > temperatures[stack.peek()]) {
+                ans[stack.peek()] = i - stack.peek();
+                stack.pop();
+            }
+            stack.push(i);
+        }
+        
+        return ans;
+    }
+}
