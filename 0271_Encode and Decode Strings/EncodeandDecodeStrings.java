7fc36ad211c63febc80e9791ec41eccaaa8677e1
Jiajionline <Jiajionline@gmail.com>
2023-03-06 06:08:15 -0800

    Leetcode 271 Encode and Decode Strings

--- 0271_Encode and Decode Strings/EncodeandDecodeStrings.java
+++ 0271_Encode and Decode Strings/EncodeandDecodeStrings.java
@@ -1,43 +1,45 @@
-// from leetcode solution
 public class Codec {
-  // Encodes string length to char array
-  public char[] intToChars(String s) {
-    int x = s.length();
-    char[] chars = new char[4];
-    for(int i = 3; i > -1; --i) {
-      chars[3 - i] = (char) (x >> (i * 8) & 0xff);
-    }
-    return chars;
-  }
 
   // Encodes a list of strings to a single string.
   public String encode(List<String> strs) {
-    StringBuilder sb = new StringBuilder();
-    for(String s: strs) {
-      sb.append(intToChars(s));
-      sb.append(s);
-    }
-    return sb.toString();
-  }
-
-  // Decodes bytes string to integer
-  public int stringToInt(String str) {
-    int result = 0;
-    for(char b : str.toCharArray())
-      result = (result << 8) + (int)b;
-    return result;
+      StringBuilder sb = new StringBuilder();
+      for(String s : strs){
+          sb.append(intToString(s.length()));
+          sb.append(s.toCharArray());
+      }
+      
+      return sb.toString();
   }
 
   // Decodes a single string to a list of strings.
   public List<String> decode(String s) {
-    int i = 0, n = s.length();
-    List<String> ans = new ArrayList();
-    while (i < n) {
-      int length = stringToInt(s.substring(i, i + 4));
-      i += 4;
-      ans.add(s.substring(i, i + length));
-      i += length;
-    }
-    return ans;
+      List<String> list = new ArrayList<>();
+      if(s.isEmpty()) return list;
+      int index = 0;
+      while(index < s.length()){
+          int len = stringToInt(s.substring(index, index+4));
+          index+=4;
+          list.add(s.substring(index, index+len));
+          index+=len;
+      }
+      
+      return list;
+  }
+  
+  private char[] intToString(int x) {
+      char[] chars = new char[4];
+      for(int i=3;i>=0;i--){
+          chars[3-i] = (char)((x >> i * 8) & 0xFF);
+      }
+      
+      return chars;
+  }
+  
+  private int stringToInt(String s) {
+      int result = 0;
+      for(char c : s.toCharArray()) {
+          result = (result << 8) + (int)c;
+      }
+      return result;
   }
 }
