42e766cbef47747afb81179a816691dbfba10de8
Jiajionline <Jiajionline@gmail.com>
2023-06-27 06:02:04 -0700

    Leetcode 101 Symmetric Tree

---
+++ 0101_Symmetric Tree/SymmetricTree2.java
@@ -0,0 +1,12 @@
+class Solution {
+    public boolean isSymmetric(TreeNode root) {
+        if(root == null) return true;
+        return isSymmetric(root.left, root.right);
+    }
+
+    private boolean isSymmetric(TreeNode node1, TreeNode node2) {
+        if(node1 == null && node2 == null) return true;
+        if(node1 == null || node2 == null || node1.val != node2.val) return false;
+        return isSymmetric(node1.left, node2.right) && isSymmetric(node1.right, node2.left);
+    }
+}
