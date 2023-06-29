f46985d8c739a11560ac747b5abc2fcf3b997a59
Jiajionline <Jiajionline@gmail.com>
2023-06-29 09:43:36 -0700

    Leetcode 191 Number of 1 Bits

--- 0191_Number of 1 Bits/Numberof1Bits.java
+++ 0191_Number of 1 Bits/Numberof1Bits.java
@@ -5,7 +5,7 @@ public class Solution {
         int ones = 0;
         while (n != 0) {
             ones = ones + (n & 1);
-            n = n >> 1;
+            n = n >>> 1;
         }
         return ones;
     }
