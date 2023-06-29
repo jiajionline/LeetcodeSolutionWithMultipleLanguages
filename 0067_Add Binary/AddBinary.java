b250415b52832f6ac9994649ef385f1f7595b6c1
Jiajionline <Jiajionline@gmail.com>
2023-06-29 06:24:38 -0700

    Leetcode 67 Add Binary

--- 0067_Add Binary/AddBinary.java
+++ 0067_Add Binary/AddBinary.java
@@ -1,31 +1,26 @@
 ﻿class Solution {
     public String addBinary(String a, String b) {
-        StringBuilder sb = new StringBuilder();
-        
-        char[] chars_a = a.toCharArray();
-        char[] chars_b = b.toCharArray();
-        
-        int p1 = chars_a.length - 1;
-        int p2 = chars_b.length - 1;
+        int p1 = a.length() - 1;
+        int p2 = b.length() - 1;
         int carry = 0;
-        
+        StringBuilder sb = new StringBuilder();
+
         while(p1 >= 0 || p2 >= 0 || carry > 0) {
-            int val = 0;
-            if(p1 >= 0)
-                val += (chars_a[p1--] - '0');
-            
-            if(p2 >= 0)
-                val += (chars_b[p2--] - '0');
-            
-            val += carry;
-            
-            carry = val / 2;
-            val %=2;
-            
-            
-            sb.insert(0, val);
+            int v = carry;
+            carry = 0;
+            if(p1 >= 0) {
+                v += a.charAt(p1--) - '0';
+            }
+
+            if(p2 >= 0) {
+                v += b.charAt(p2--) - '0';
+            }
+
+            carry = v / 2;
+            v %= 2;
+            sb.insert(0, v);
         }
-        
+
         return sb.toString();
     }
 }
