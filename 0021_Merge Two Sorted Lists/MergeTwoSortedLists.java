0ec8623e4477d7dafea1b0bc401172caed911c20
Jiajionline <Jiajionline@gmail.com>
2023-07-01 07:39:00 -0700

    Leetcode 21 Merge Two Sorted Lists

--- 0021_Merge Two Sorted Lists/MergeTwoSortedLists.java
+++ 0021_Merge Two Sorted Lists/MergeTwoSortedLists.java
@@ -2,12 +2,29 @@ class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
         if(list1 == null) return list2;
         if(list2 == null) return list1;
-        if(list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);
-            return list1;
-        }else{
-            list2.next = mergeTwoLists(list1, list2.next);
-            return list2;
+        
+        ListNode preHead = new ListNode();
+        ListNode curr = preHead;
+
+        ListNode p1 = list1;
+        ListNode p2 = list2;
+        
+        while(p1!=null && p2!=null)
+        {
+            if(p1.val <= p2.val){
+                curr.next = p1;
+                p1 = p1.next;
+            }else{
+                curr.next = p2;
+                p2 = p2.next;
+            }
+            
+            curr = curr.next;
         }
+        
+        if(p1!=null) curr.next = p1;
+        if(p2!=null) curr.next = p2;
+        
+        return preHead.next;
     }
 }
