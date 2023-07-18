4d05953a9348d99cefbe9bfd94a82daf2ad1629f
Jiajionline <Jiajionline@gmail.com>
2023-07-18 06:55:54 -0700

    Leetcode 33 Search in Rotated Sorted Array

--- 0033_Search in Rotated Sorted Array/SearchinRotatedSortedArray.java
+++ 0033_Search in Rotated Sorted Array/SearchinRotatedSortedArray.java
@@ -8,12 +8,12 @@ public class Solution {
                 return mid;
         
             if (nums[l] <= nums[mid]){
-                 if (target < nums[mid] && target >= nums[l]) 
+                 if (nums[l] <= target && target < nums[mid]) 
                     r = mid - 1;
                  else
                     l = mid + 1;
             }else{
-                if (target > nums[mid] && target <= nums[r])
+                if (nums[mid] < target && target <= nums[r])
                     l = mid + 1;
                  else
                     r = mid - 1;
