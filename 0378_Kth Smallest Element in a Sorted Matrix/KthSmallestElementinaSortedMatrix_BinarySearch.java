a1d7dc3461b08357d57f05a1b362dedd14277224
Jiajionline <Jiajionline@gmail.com>
2023-07-23 09:31:07 -0700

    Leetcode 378 Kth Smallest Element in a Sorted Matrix

--- 0378_Kth Smallest Element in a Sorted Matrix/KthSmallestElementinaSortedMatrix_BinarySearch.java
+++ 0378_Kth Smallest Element in a Sorted Matrix/KthSmallestElementinaSortedMatrix_BinarySearch.java
@@ -1,35 +1,32 @@
-public class Solution {
+class Solution {
     public int kthSmallest(int[][] matrix, int k) {
-        //[l, r), lower bound
-        int l = matrix[0][0];
-        int r = matrix[matrix.length - 1][matrix[0].length - 1] + 1;
+        int m = matrix.length, n = matrix[0].length;
+        int l = matrix[0][0], r = matrix[m-1][n-1] + 1;
         while(l < r) {
-            int mid = l + (r - l) / 2;
-            int total = 0;
-            int j = matrix[0].length - 1;
-            if (equalOrSmaller(matrix,mid)<k)
-                l = mid+1;
-            else
+            int mid = (r-l)/2 + l;
+            int count = smallerOrEqual(matrix, mid);
+            if(count >= k) {
                 r = mid;
+            }else{
+                l = mid + 1;
+            }
         }
+
         return l;
     }
-    
-    private int equalOrSmaller(int[][] matrix, int val)
-    {
-        int n = matrix.length;
-        int m = matrix[0].length;
-        int i=n-1, j=0, count = 0;
-        while (i>=0 && j<m)
-        {
-            if (matrix[i][j]<=val)
-            {
-                count+=(i+1);
-                j++;
+
+    private int smallerOrEqual(int[][] matrix, int v) {
+        int count = 0;
+        int row = matrix.length-1, col = 0;
+        while(row>=0 && col < matrix[0].length) {
+            if(matrix[row][col] <= v){
+                count+= (row+1);
+                col++;
+            }else{
+                row--;
             }
-            else
-                i--;                
         }
+
         return count;
     }
 }
