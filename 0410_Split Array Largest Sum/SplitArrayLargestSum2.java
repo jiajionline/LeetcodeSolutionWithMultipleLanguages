61f353839a493b1c2305e08fde258b4ae6e80b32
Jiajionline <Jiajionline@gmail.com>
2023-07-24 16:04:49 -0700

    Leetcode 410 Split Array Largest Sum

--- 0410_Split Array Largest Sum/SplitArrayLargestSum2.java
+++ 0410_Split Array Largest Sum/SplitArrayLargestSum2.java
@@ -1,5 +1,5 @@
 class Solution {
-    public int splitArray(int[] nums, int m) {
+    public int splitArray(int[] nums, int k) {
         int n = nums.length;
         int[] presum = new int[n];
         presum[0] = nums[0];
@@ -8,22 +8,20 @@ class Solution {
             presum[i] = nums[i] + presum[i - 1];
         }
 
-        int[][] dp = new int[m+1][n];
+        int[][] dp = new int[k + 1][n];
         for (int i = 0; i < dp.length; i++) {
-            for (int j = 0; j < dp[i].length; j++) {
-                dp[i][j] = Integer.MAX_VALUE;
-            }
+            Arrays.fill(dp[i], Integer.MAX_VALUE);
         }
 
-        for(int i=0;i<n;i++){
+        for (int i = 0; i < n; i++) {
             dp[1][i] = presum[i];
         }
 
-        for (int i = 2; i <= m; ++i)
-        for (int j = i - 1; j < n; ++j)
-          for (int k = 0; k < j; ++k)
-            dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][k], presum[j] - presum[k]));
-     
-        return dp[m][n - 1];
+        for (int i = 2; i <= k; ++i)
+            for (int j = i - 1; j < n; ++j)
+                for (int z = 0; z < j; ++z)
+                    dp[i][j] = Math.min(dp[i][j], Math.max(dp[i - 1][z], presum[j] - presum[z]));
+
+        return dp[k][n - 1];
     }
 }
