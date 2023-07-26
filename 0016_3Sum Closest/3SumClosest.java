03b3982d8da8c60fdcdaab2627e084a1d71f15e3
Jiajionline <Jiajionline@gmail.com>
2023-07-25 18:46:35 -0700

    Leetcode 16 3Sum Closest

--- 0016_3Sum Closest/3SumClosest.java
+++ 0016_3Sum Closest/3SumClosest.java
@@ -1,21 +1,20 @@
 class Solution {
     public int threeSumClosest(int[] nums, int target) {
         Arrays.sort(nums);
-        int n = nums.length;
-        int ans = nums[0] + nums[1] + nums[2];
-        for(int i=0;i<n-2;i++) {
-            int l = i+1, r = n - 1;
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length-2;i++) {
+            int l = i+1, r = nums.length - 1;
             while(l < r) {
                 int sum = nums[i] + nums[l] + nums[r];
-                if(sum == target) return target;
-                else if(sum > target) r--;
-                else l++;
-                if(Math.abs(sum - target) < Math.abs(ans - target)){
+                if(Math.abs(sum - target) < Math.abs(ans - target)) {
                     ans = sum;
-                }    
+                }
+
+                if(sum > target) r--;
+                else l++;
             }
         }
-        
+
         return ans;
     }
 }
