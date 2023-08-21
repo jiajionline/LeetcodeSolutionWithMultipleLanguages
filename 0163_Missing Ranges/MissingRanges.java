b826167b2c13a065cef76cb37b8b8feefe809de5
Jiajionline <Jiajionline@gmail.com>
2023-08-21 06:50:08 -0700

    Leetcode 163 Missing Ranges

--- 0163_Missing Ranges/MissingRanges.java
+++ 0163_Missing Ranges/MissingRanges.java
@@ -1,21 +1,19 @@
 class Solution {
-    public List<String> findMissingRanges(int[] nums, int lower, int upper) {
-        List<String> ans = new ArrayList<>();
-        int prev = lower - 1;
-        for (int i = 0; i <= nums.length; i++) {
-            int curr = (i < nums.length) ? nums[i] : upper + 1;
-            if (prev + 1 <= curr - 1) {
-                ans.add(range(prev + 1, curr - 1));
+    public List<List<Integer>> findMissingRanges(int[] nums, int lower, int upper) {
+        List<List<Integer>> ans = new ArrayList<>();
+        int prev = lower-1;
+        for(int i=0;i<=nums.length;i++) {
+            int curr = i < nums.length ? nums[i] : upper + 1;
+            if(prev + 1 <= curr - 1) {
+                List<Integer> list = new ArrayList<>();
+                list.add(prev + 1);
+                list.add(curr - 1);
+                ans.add(list);
             }
             prev = curr;
-        }
-        return ans;
-    }
 
-    private String range(int lower, int upper) {
-        if (lower == upper) {
-            return String.valueOf(lower);
         }
-        return lower + "->" + upper;
+        
+        return ans;
     }
 }
