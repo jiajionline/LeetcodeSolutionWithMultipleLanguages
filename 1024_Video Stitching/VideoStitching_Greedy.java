1e0f358b950dbc523554d4b06075978e246562d2
Jiajionline <Jiajionline@gmail.com>
2023-08-24 06:46:33 -0700

    Leetcode 1024 Video Stitching

--- 1024_Video Stitching/VideoStitching_Greedy.java
+++ 1024_Video Stitching/VideoStitching_Greedy.java
@@ -1,27 +1,23 @@
 class Solution {
     public int videoStitching(int[][] clips, int time) {
         Arrays.sort(clips, (a,b)-> Integer.compare(a[0], b[0]));
-        int curr = 0;
-        int ans = 0;
-        int i = 0;
-        while(i < clips.length) {
+        int ans = 0, curr = 0, index = 0;
+        while(index < clips.length) {
             int far = curr;
             // if the start of the clip is less and equals to the curr, 
             // so the clip overlapped with its previous, we can decide the far
-            while(i < clips.length && clips[i][0] <= curr) {
-                far = Math.max(far, clips[i][1]);
-                i++;
+            while(index < clips.length && clips[index][0] <= curr){
+                far = Math.max(far, clips[index][1]);
+                index++;
             }
-            
             ans++;
-            
             if(far >= time) return ans;
             // can't move anymore like [0,2] <- curr, far  [4,6]
-            else if(far == curr) return -1;
+            else if(curr == far) return -1;
             
             curr = far;
         }
-        
+        // no answer found
         return -1;
     }
 }
