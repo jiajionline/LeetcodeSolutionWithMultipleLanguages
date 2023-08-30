e6c6838f99b7aa6c9f5291f1e8fb64a1ae9d553e
Jiajionline <Jiajionline@gmail.com>
2023-08-30 10:30:04 -0700

    Leetcode 1558 Minimum Numbers of Function Calls to Make Target Array

--- 1558_Minimum Numbers of Function Calls to Make Target Array/MinimumNumbersofFunctionCallstoMakeTargetArray.java
+++ 1558_Minimum Numbers of Function Calls to Make Target Array/MinimumNumbersofFunctionCallstoMakeTargetArray.java
@@ -1,10 +1,11 @@
 class Solution {
     public int minOperations(int[] nums) {
         int countOf1 = 0;
-        int highest = 0;
+        int maxShift = 0;
         
         for(int v : nums) {
-            int h = -1;
+            // h to check the highest 1
+            int h = 0;
             while(v!=0) {
                 if((v & 1) == 1) {
                     countOf1++;
@@ -12,10 +13,13 @@ class Solution {
                 h++;
                 v >>= 1;
             }
+
+            // the count of shift left ( X2 operation), need to minus 1 since the first 1 not needed to X2
+            h--;
             
-            highest = Math.max(highest, h);
+            maxShift = Math.max(maxShift, h);
         }
         
-        return countOf1 + highest;
+        return countOf1 + maxShift;
     }
 }
