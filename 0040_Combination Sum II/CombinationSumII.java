ff42ba2bff98c0960ded04515c24c8a8d71f9998
Jiajionline <Jiajionline@gmail.com>
2023-09-01 09:22:10 -0700

    Leetcode 40 Combination Sum II

--- 0040_Combination Sum II/CombinationSumII.java
+++ 0040_Combination Sum II/CombinationSumII.java
@@ -1,27 +1,23 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
-        List<List<Integer>> ans = new ArrayList<List<Integer>>();
-        
+        List<List<Integer>> ans = new ArrayList<>();
         Arrays.sort(candidates);
-        DFS(candidates, target, 0, new ArrayList<Integer>(), ans);
+        backtracking(candidates, target, 0, new ArrayList<Integer>(), ans);
         return ans;
-        
     }
-    
-    private void DFS(int[] candidates, int target, int s, List<Integer> curr, List<List<Integer>> ans){
-        if(target == 0){
-                ans.add(new ArrayList<Integer>(curr));
-                return;
-            }
-               
-        for(int i=s;i<candidates.length;i++){
-             
+
+    private void backtracking(int[] candidates, int target, int index, List<Integer> list, List<List<Integer>> ans) {
+        if(target == 0) {
+            ans.add(new ArrayList<Integer>(list));
+            return;
+        }
+
+        for(int i=index;i<candidates.length;i++) {
+            if(i > index && candidates[i] == candidates[i-1]) continue;
             if(candidates[i] > target) break;
-            if(i>s && candidates[i] == candidates[i-1]) continue;
-            
-            curr.add(candidates[i]);
-            DFS(candidates, target - candidates[i], i+1, curr, ans);
-            curr.remove(curr.size()-1);
+            list.add(candidates[i]);
+            backtracking(candidates, target - candidates[i], i+1, list, ans);
+            list.remove(list.size() - 1);
         }
     }
 }
