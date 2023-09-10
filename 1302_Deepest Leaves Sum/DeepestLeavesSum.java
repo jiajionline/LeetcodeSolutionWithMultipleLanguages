5e842d2accd00e38372373408529f76492000c7d
Jiajionline <Jiajionline@gmail.com>
2023-09-10 06:55:36 -0700

    Leetcode 1302 Deepest Leaves Sum

--- 1302_Deepest Leaves Sum/DeepestLeavesSum.java
+++ 1302_Deepest Leaves Sum/DeepestLeavesSum.java
@@ -1,12 +1,3 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode(int x) { val = x; }
- * }
- */
 class Solution {
     public int deepestLeavesSum(TreeNode root) {
         List<Integer> levelToSum = new ArrayList<Integer>();
