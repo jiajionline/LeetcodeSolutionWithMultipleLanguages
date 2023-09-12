9c6ca272eab5f09d27f2a6b1ea3e07781a604a6e
Jiajionline <Jiajionline@gmail.com>
2023-09-12 05:17:42 -0700

    Leetcode 72 Edit Distance

--- 0072_Edit Distance/EditDistance.java
+++ 0072_Edit Distance/EditDistance.java
@@ -1,28 +1,23 @@
-public class Solution{
-
-    public static int minDistance(String word1, String word2){
-        int m = word1.length();
-        int n = word2.length();
-        int[][] dp = new int[m + 1][n + 1];
-        for (int i = 0; i <= m; i++){
-            dp[i][0] = i;
-        }
-        for (int j = 0; j <= n; j++){
-            dp[0][j] = j;
-        }
-        for (int j = 1; j <= n; j++){
-            for (int i = 1; i <= m; i++){
-                if (word1.charAt(i - 1) == word2.charAt(j - 1)){
-                    dp[i][j] = dp[i - 1][j - 1];
-                } else{
-                    dp[i][j] = min((dp[i - 1][j] + 1), (dp[i][j - 1] + 1), (dp[i - 1][j - 1] + 1));
+class Solution {
+    public int minDistance(String word1, String word2) {
+        int m = word1.length(), n = word2.length();
+        int[][] dp = new int[m+1][n+1];
+        for(int i=0;i<=m;i++) {
+            for(int j=0;j<=n;j++) {
+                // when word1 is empty
+                if(i == 0) {
+                    dp[i][j] = j;
+                // when word2 is empty 
+                }else if(j == 0){
+                    dp[i][j] = i;
+                }else if(word1.charAt(i-1) == word2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1];
+                }else{
+                    dp[i][j] = Math.min(dp[i-1][j-1], Math.min(dp[i][j-1], dp[i-1][j])) + 1;
                 }
             }
         }
-        return (dp[m][n]);
-    }
 
-    public static int min(int a, int b, int c){
-        return (Math.min(Math.min(a, b), c));
+        return dp[m][n];
     }
 }
