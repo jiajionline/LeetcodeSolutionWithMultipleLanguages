ec3fd86977f79f36f42a91d77c6f526663b78ff0
Jiajionline <Jiajionline@gmail.com>
2023-09-13 16:14:43 -0700

    Leetcode 845 Longest Mountain in Array

--- 0845_Longest Mountain in Array/LongestMountaininArray_O(n)_2.java
+++ 0845_Longest Mountain in Array/LongestMountaininArray_O(n)_2.java
@@ -1,21 +1,17 @@
 class Solution {
     public int longestMountain(int[] arr) {
-        int n = arr.length;
-        int ans = 0 , l = 0 , r = 0;
-        // check if out of boundary, the range must >= 3
-        while(l + 2 < n) {
-            r = l + 1;
-            if(arr[l] < arr[r]) {
-                // check if r+1 satisfy 
-                while(r + 1 < n && arr[r] < arr[r+1]) r++;
+        int ans = 0, l = 0, n = arr.length;
+        while(l < n) {
+            int r = l + 1;
+            if(r < n && arr[l] < arr[r]) {
+                while(r+1 < n && arr[r] < arr[r+1]) r++;
                 if(r+1 < n && arr[r] > arr[r+1]) {
-                    while(r + 1 < n && arr[r] > arr[r+1]) r++;
-                    ans = Math.max(ans, r - l + 1);
+                    while(r+1 < n && arr[r] > arr[r+1]) r++;
+                    ans = Math.max(ans, r-l + 1);
                 }
             }
             l = r;
         }
-        
-        return ans;
+        return ans >= 3 ? ans : 0;
     }
 }
