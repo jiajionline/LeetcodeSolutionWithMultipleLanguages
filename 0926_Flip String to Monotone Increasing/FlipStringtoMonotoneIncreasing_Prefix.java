e11c362c354707637063a16786e0ef68f18e0615
Jiajionline <Jiajionline@gmail.com>
2023-09-13 16:15:00 -0700

    Leetcode 926 Flip String to Monotone Increasing

--- 0926_Flip String to Monotone Increasing/FlipStringtoMonotoneIncreasing_Prefix.java
+++ 0926_Flip String to Monotone Increasing/FlipStringtoMonotoneIncreasing_Prefix.java
@@ -1,1 +1,24 @@
-t st
+class Solution {
+    public int minFlipsMonoIncr(String s) {
+        int n = s.length();
+        int[] l = new int[n];
+        int[] r = new int[n];
+        l[0] = s.charAt(0) == '0' ? 0 : 1;
+        r[n-1] = s.charAt(n-1) == '1' ? 0 : 1;
+
+        for(int i=1;i<n;i++) {
+            l[i] = s.charAt(i) == '1' ? l[i-1] + 1 : l[i-1];
+        }
+
+        for(int i=n-2;i>=0;i--) {
+            r[i] = s.charAt(i) == '0' ? r[i+1] + 1 : r[i+1];
+        }
+
+        // the case all 0s or 1s
+        int ans = Math.min(r[0], l[n-1]);
+        for(int i=1;i<n;i++) {
+            ans = Math.min(ans, l[i-1] + r[i]);
+        }
+        return ans;
+    }
+}
