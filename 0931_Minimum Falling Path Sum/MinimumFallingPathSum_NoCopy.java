184c152b5af0e9345a508824e698c95d8a285b07
Jiajionline <Jiajionline@gmail.com>
2023-09-27 06:10:03 -0700

    Leetcode 931 Minimum Falling Path Sum

--- 0931_Minimum Falling Path Sum/MinimumFallingPathSum_NoCopy.java
+++ 0931_Minimum Falling Path Sum/MinimumFallingPathSum_NoCopy.java
@@ -1,27 +1,19 @@
 class Solution {
     public int minFallingPathSum(int[][] matrix) {
-        int m = matrix.length;
-        int n = matrix[0].length;
-        
-        for(int i=m-2;i>=0;i--)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(j == 0) {
-                    matrix[i][j] += Math.min(matrix[i+1][j], matrix[i+1][j+1]);
-                }else if(j == n-1) {
-                    matrix[i][j] += Math.min(matrix[i+1][j-1], matrix[i+1][j]);
-                }else{
-                    matrix[i][j] += Math.min(matrix[i+1][j-1], Math.min(matrix[i+1][j], matrix[i+1][j+1]));
-                }
+        int m = matrix.length, n = matrix[0].length;
+        for(int row=1;row<m;row++) {
+            for(int col=0;col<n;col++) {
+                   int leftTop = col == 0 ? Integer.MAX_VALUE : matrix[row-1][col-1];
+                   int top = matrix[row-1][col];
+                   int rightTop = col == n-1 ? Integer.MAX_VALUE : matrix[row-1][col+1];
+                   matrix[row][col] += ( Math.min(leftTop, Math.min(top, rightTop)));
             }
         }
-        
-        int ans = matrix[0][0];
-        
-        for(int i=1;i<n;i++)
-            ans = Math.min(ans, matrix[0][i]);
-        
+
+        int ans = Integer.MAX_VALUE;
+        for(int i=0;i<n;i++) {
+            ans = Math.min(ans, matrix[m-1][i]);
+        }
         return ans;
     }
 }
